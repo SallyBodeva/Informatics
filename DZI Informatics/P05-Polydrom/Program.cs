46180ceb928c998e669313e46b62f760ac2f90f0
salihe <salihe.bodeva10@gmail.com>
2023-09-30 23:07:13 +0300

    DZI

---
+++ DZI Informatics/P05-Polydrom/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Linq;
+
+namespace P05_Polydrom
+{
+    public class Program
+    {
+        static void Main()
+        {
+            string num = Console.ReadLine();
+            int n = 0;
+            int indexMiddle = 0;
+            try
+            {
+                n = int.Parse(num);
+                if (n < 0)
+                {
+                    Console.WriteLine("Incorrectly entered number"); 
+                    Environment.Exit(0);
+                }
+            }
+            catch
+            {
+                Console.WriteLine("Incorrectly entered number");
+                Environment.Exit(0);
+            }
+            if (n % 2 == 0)
+            {
+                indexMiddle = (num.Length / 2) - 1;
+            }
+            else
+            {
+                indexMiddle = num.Length / 2;
+            }
+
+            string firstPart = num.Substring(0,(num.Length-indexMiddle));
+            string seconfPart = new string(num.Substring(indexMiddle, num.Length - (indexMiddle)).Reverse().ToArray());
+            if (firstPart==seconfPart)
+            {
+                Console.WriteLine($"{num} is a palindrome");
+            }
+            else
+            {
+                Console.WriteLine($"{num} is NOT a palindrome");
+            }
+        }
+    }
+}
