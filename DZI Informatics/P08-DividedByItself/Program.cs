0b8c95f6439ca8fd02bc93989279a63281d453dc
salihe <salihe.bodeva10@gmail.com>
2023-10-02 20:30:56 +0300

    DZI

---
+++ DZI Informatics/P08-DividedByItself/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace P08_DividedByItself
+{
+    public class Program
+    {
+        static void Main()
+        {
+            string num = Console.ReadLine();
+            int number = int.Parse(num);
+            int digits = num.Length;
+            int count = 0;
+            for (int i = 0; i < num.Length; i++)
+            {
+                if (num[i]=='-')
+                {
+                    digits--;
+                    i++;
+                }
+                int n = int.Parse(num[i].ToString());
+                if (number % n==0)
+                {
+                    count += 1;
+                }
+            }
+            if (count==digits)
+            {
+                Console.WriteLine("Yes");
+            }
+            else
+            {
+                Console.WriteLine("No");
+            }
+        }
+    }
+}
