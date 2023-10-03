7469a0feb833ae9b72a418f3643c6a3d765beaf9
salihe <salihe.bodeva10@gmail.com>
2023-10-03 18:22:02 +0300

    DZI

---
+++ DZI Informatics/P11-ToTheMoon/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace P11_ToTheMoon
+{
+    public class Program
+    {
+        static void Main()
+        {
+            double speed = double.Parse(Console.ReadLine());
+            double fuelPer100 = double.Parse(Console.ReadLine());
+            double km = 768800;
+            double time = Math.Ceiling(km / speed)+3;
+            Console.WriteLine(time);
+            double fuel = (km * fuelPer100) / 100;
+            Console.WriteLine(fuel);
+        }
+    }
+}
