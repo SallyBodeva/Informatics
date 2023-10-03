3d54323b9382542661649ea15d85415901976e39
salihe <salihe.bodeva10@gmail.com>
2023-10-03 20:58:17 +0300

    DZI

---
+++ DZI Informatics/P15-NumbersCount/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P15_NumbersCount
+{
+    public class Program
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            Dictionary<int, int> nums = new Dictionary<int, int>();
+            for (int i = 0; i < n; i++)
+            {
+                int number = int.Parse(Console.ReadLine());
+                if (nums.ContainsKey(number))
+                {
+                    nums[number] += 1;
+                }
+                else
+                {
+                    nums.Add(number, 1);
+                }
+            }
+            foreach (var item in nums.OrderByDescending(x=>x.Value))
+            {
+                Console.WriteLine($"число: {item.Key}, брой: {item.Value}");
+            }
+        }
+    }
+}
