a365644cdd3c0cc4c3e9fea8e36a7f887ef15f26
salihe <salihe.bodeva10@gmail.com>
2023-10-04 22:18:55 +0300

    DZI

---
+++ DZI Informatics/P17-Methods/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P17_Methods
+{
+    public class Program
+    {
+        static void Main()
+        {
+            List<int> list = Console.ReadLine().Split(" ").Select(int.Parse).ToList();
+            Console.WriteLine(String.Join(" ",DividedByK(list,3)));
+        }
+        public static List<int> DividedByK(List<int> list, int k)
+        {
+            for (int i = 0; i < list.Count; i++)
+            {
+                int n = list[i];
+                int sumN = 0;
+                while (n > 0)
+                {
+                    sumN += n % 10;
+                    n /= 10;
+                }
+                if (sumN % k == 0)
+                {
+                    list.RemoveAt(i);
+                }
+            }
+            return list;
+        }
+       
+    }
+}
