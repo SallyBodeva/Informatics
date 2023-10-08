6127f20ac7d7de0362b869eaccbcee5124a0e89f
salihe <salihe.bodeva10@gmail.com>
2023-10-08 21:24:51 +0300

    DZI

---
+++ DZI Informatics/P20-SolarSystem/Program.cs
@@ -0,0 +1,32 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P20_SolarSystem
+{
+    public class Program
+    {
+        static void Main()
+        {
+            List<Star> stars = new List<Star>()
+            {
+              new Star("Test",2.1,Classification.,500,"Milky Way"),
+              new Star("Test1",5.1,Classification.кафявиджуджета,1000,"Milky Way1"),
+              new Star("Test2",1.5,Classification.субджуджета,250,"Milky Way2"),
+              new Star("Test3",9.1,Classification.гиганти,1900,"Milky Way3")
+            };
+            Console.WriteLine(string.Join("\n",SortedStars(stars)));
+        }
+        public static List<string> SortedStars(List<Star> list)
+        {
+            List<string> output = new List<string>();
+            foreach (var item in list.OrderBy(x=>x.Distance))
+            {
+                string text = $"{item.Name}, {item.Distance} св.г., {item.Classification}, {item.Weight} сл.м., {item.Constellation}";
+                output.Add(text);
+            }
+            return output;
+        }
+    }
+}
---
+++ DZI Informatics/P20-SolarSystem/Star.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P20_SolarSystem
+{
+    public class Star
+    {
+        private string name;
+        private double distance;
+
+        public Star(string name, double distance, int classification, double weight, string constellation)
+        {
+            this.Name = name;
+            this.Distance = distance;
+            this.Classification = classification;
+            this.Weight = weight;
+            this.Constellation = constellation;
+        }
+
+        public string Name
+        {
+            get => name;
+            private set
+            {
+                if (value.Length > 20)
+                {
+                    throw new ArgumentException("Invalid name...");
+                }
+                name = value;
+            }
+        }
+        public double Distance
+        {
+            get => distance; private set
+            {
+                if (value<0)
+                {
+                    throw new ArgumentException("Invalid distance...");
+                }
+                distance = value;
+            }
+        }
+        public int Classification { get; set; }
+        public double Weight { get; set; }
+        public string Constellation { get; set; }
+    }
+}
