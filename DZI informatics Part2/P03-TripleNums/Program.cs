a77b7e9328b63fdefb15e5438da8f90587c921b5
salihe <salihe.bodeva10@gmail.com>
2023-10-15 22:49:57 +0300

    DZI

---
+++ DZI informatics Part2/P03-TripleNums/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P03_TripleNums
+{
+    public class Program
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<Triple> numbers = new List<Triple>();
+            for (int i = 0; i < n; i++)
+            {
+                int[] num= Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+                Triple triple = new Triple(num[0], num[1], num[2]);
+                numbers.Add(triple);
+            }
+            foreach (var item in numbers.OrderBy(x => x.Num1).ThenBy(x => x.Num2).ThenBy(x => x.Num3))
+            {
+                Console.WriteLine($"{item.Num1} {item.Num2} {item.Num3}");
+            }
+        }
+    }
+}
---
+++ DZI informatics Part2/P03-TripleNums/Triple.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P03_TripleNums
+{
+    public class Triple
+    {
+        public Triple(int num1, int num2, int num3)
+        {
+            this.Num1 = num1;
+            this.Num2 = num2;
+            this.Num3 = num3;
+        }
+
+        public int Num1 { get; set; }
+        public int Num2 { get; set; }
+        public int Num3 { get; set; }
+    }
+}
