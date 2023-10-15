09ba1d02c3fa33ba90cf77ace7ffdad250f87e75
salihe <salihe.bodeva10@gmail.com>
2023-10-15 19:47:29 +0300

    DZI

---
+++ DZI informatics Part2/P01-SimilarNums/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P01_SimilarNums
+{
+    public class Program
+    {
+        static void Main()
+        {
+            int[] array1 = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+            int[] array2 = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+            List<int> outout = new List<int>();
+            foreach (var item in array2)
+            {
+                if (array1.Contains(item))
+                {
+                    outout.Add(item);
+                }
+            }
+            Console.WriteLine(String.Join(" ",outout.OrderBy(x=>x)));
+        }
+    }
+}
---
+++ DZI informatics Part2/P02-PersonalNumsSort/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace P02_PersonalNumsSort
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<KeyValuePair<int,int>> pairs = new List<KeyValuePair<int, int>>();
+            for (int i = 0; i < n; i++)
+            {
+                int[] input = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+                KeyValuePair<int,int> keyValuePair = new KeyValuePair<int,int>(input[0],input[1]);
+                pairs.Add(keyValuePair);
+            }
+            foreach (var item in pairs.OrderBy(x=>x.Key).ThenBy(x=>x.Value))
+            {
+                Console.WriteLine($"{item.Key} {item.Value}");
+            }
+        }
+    }
+}
