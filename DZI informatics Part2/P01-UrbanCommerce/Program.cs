2e7e21a9b9b5b34e151c6aecfc3e14d56d131068
salihe <salihe.bodeva10@gmail.com>
2023-10-15 19:22:35 +0300

    DZI

---
+++ DZI informatics Part2/P01-UrbanCommerce/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace P01_UrbanCommerce
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
