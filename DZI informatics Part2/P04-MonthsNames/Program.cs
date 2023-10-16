ff8ea4e8b2d817eb17c1372fc8b289965cf915d8
salihe <salihe.bodeva10@gmail.com>
2023-10-16 20:23:09 +0300

    DZI

---
+++ DZI informatics Part2/P04-MonthsNames/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Linq.Expressions;
+
+namespace P04_MonthsNames
+{
+    public class Program
+    {
+        static void Main()
+        {
+            while (true)
+            {
+                int sum = 0;
+                string letter = Console.ReadLine().ToLower();
+                if (!char.IsLetter(letter.ToCharArray()[0]))
+                {
+                    break;
+                }
+                if ("january".Contains(letter)) sum++;
+                if ("february".Contains(letter)) sum++;
+                if ("march".Contains(letter)) sum++;
+                if ("april".Contains(letter)) sum++;
+                if ("may".Contains(letter)) sum++;
+                if ("june".Contains(letter)) sum++;
+                if ("july".Contains(letter)) sum++;
+                if ("august".Contains(letter)) sum++;
+                if ("semptember".Contains(letter)) sum++;
+                if ("october".Contains(letter)) sum++;
+                if ("november".Contains(letter)) sum++;
+                if ("december".Contains(letter)) sum++;
+                Console.WriteLine(sum);
+            }
+        }
+    }
+}
