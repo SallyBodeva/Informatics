68e215c1a6dfdf56648fc07511200f1365a4f93b
salihe <salihe.bodeva10@gmail.com>
2023-10-16 20:59:18 +0300

    DZI

---
+++ DZI informatics Part2/P05-Distances/Distances.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P05_Distances
+{
+    public static class Distances
+    {
+        public static double Euclidean(double x1, double x2, double y1, double y2)
+        {
+            return Math.Sqrt(Math.Pow(x1 - x2, 2) + Math.Pow(y1 - y2, 2));
+        }
+        public static double Manhattan(double x1, double x2, double y1, double y2)
+        {
+            return Math.Abs(x1 - x2) + Math.Abs(y1 - y2);
+        }
+        public static double Chebyshev(double x1, double x2, double y1, double y2)
+        {
+            return Math.Max(Math.Abs(x1 - x2), Math.Abs(y1 - y2));
+        }
+    }
+}
---
+++ DZI informatics Part2/P05-Distances/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Linq;
+
+namespace P05_Distances
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                double[] nums = Console.ReadLine().Split(" ").Select(double.Parse).ToArray();
+                Console.WriteLine(Distances.Euclidean(nums[0], nums[1], nums[2], nums[3])); 
+            }
+        }
+    }
+}
