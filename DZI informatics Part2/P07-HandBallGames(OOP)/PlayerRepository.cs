96cc2a31db48a0e8c7f548eb06518a023854ba63
salihe <salihe.bodeva10@gmail.com>
2023-10-23 23:53:30 +0300

    DZI

---
+++ DZI informatics Part2/P07-HandBallGames(OOP)/PlayerRepository.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+
+namespace P07_HandBallGames_OOP_
+{
+    public class PlayerRepository
+    {
+        private readonly List<Player> models;
+        public void AddModel(Player player)
+        {
+            models.Add(player);
+        }
+        public bool RemoveModel(string name)
+        {
+            Player p = models.FirstOrDefault(x => x.Name == name);
+            return this.models.Remove(p);
+        }
+        public bool ExistsModel(string name)
+        {
+            return models.Any(x => x.Name == name);
+        }
+        public Player GetModel(string name)
+        {
+            return models.FirstOrDefault(x => x.Name == name);
+        }
+    }
+}
---
+++ DZI informatics Part2/P07-HandBallGames(OOP)/TeamRepository.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace P07_HandBallGames_OOP_
+{
+    public class TeamRepository
+    {
+        private List<Team> models;
+        public void AddModel(Team team)
+        {
+            models.Add(team);
+        }
+        public bool RemoveModel(string name)
+        {
+            Team t = models.FirstOrDefault(x => x.Name == name);
+            return models.Remove(t);
+        }
+        public bool ExistsModel(string name)
+        {
+            return this.models.Any(x => x.Name == name);
+        }
+        public Team GetModel(string name)
+        {
+            return models.FirstOrDefault(x => x.Name == name);
+        }
+    }
+}
