11615f0f1efebcfc0cff35754043e4e7abbb4949
salihe <salihe.bodeva10@gmail.com>
2023-10-26 22:21:31 +0300

    DZI

---
+++ DZI informatics Part2/P08-SortingAlgorithms/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace P08_SortingAlgorithms
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
