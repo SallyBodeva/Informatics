788ae688879c43971e320b43d186d2d7f2902142
salihe <salihe.bodeva10@gmail.com>
2023-10-28 20:49:22 +0300

    DZI

--- DZI informatics Part2/P10-FilesAndExtensions/Program.cs
+++ DZI informatics Part2/P10-FilesAndExtensions/Program.cs
@@ -1,12 +1,33 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.IO;
 
 namespace P10_FilesAndExtensions
 {
-    internal class Program
+    public class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello World!");
+            string dirPath = Console.ReadLine();
+            string[] files = Directory.GetFiles(dirPath);
+            Dictionary<string, int> filesCount = new Dictionary<string, int>();
+            foreach (var file in files)
+            {
+                int dotIndex = file.LastIndexOf('.');
+                string extension = file.Substring(dotIndex);
+                if (filesCount.ContainsKey(extension))
+                {
+                    filesCount[extension]++;
+                }
+                else
+                {
+                    filesCount.Add(extension, 1);
+                }
+            }
+            foreach (var item in filesCount)
+            {
+                Console.WriteLine($"{item.Key} - {item.Value} файла");
+            }
         }
     }
 }
