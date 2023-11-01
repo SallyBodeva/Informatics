7e7c5ee8d7370843531fa99642e18e2860daa9cf
salihe <salihe.bodeva10@gmail.com>
2023-11-01 22:18:08 +0200

    DZI

--- DZI informatics Part2/P08-SortingAlgorithms/Program.cs
+++ DZI informatics Part2/P08-SortingAlgorithms/Program.cs
@@ -6,10 +6,25 @@ namespace P08_SortingAlgorithms
     {
         static void Main()
         {
-
+            int[] example = new int[] { 6, 1, 0, 19, -2 };
+            Console.WriteLine(String.Join(" ",BubbleSort(example)));
         }
 
-
-        // To do
+        public static int[] BubbleSort(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = i+1; j <array.Length; j++)
+                {
+                    if (array[i] > array[j])
+                    {
+                        int firstElement = array[i];
+                        array[i] = array[j];
+                        array[j] = firstElement;
+                    }
+                }
+            }
+            return array;
+        }
     }
 }
