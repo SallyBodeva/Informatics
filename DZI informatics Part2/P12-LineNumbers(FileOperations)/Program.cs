bb16c0b7d17015019bcc425791cdf13470bb9cdb
salihe <salihe.bodeva10@gmail.com>
2023-11-04 21:03:22 +0200

    DZI

---
+++ DZI informatics Part2/P12-LineNumbers(FileOperations)/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.IO;
+using System.Linq;
+
+namespace P12_LineNumbers_FileOperations_
+{
+    public class Program
+    {
+        static void Main()
+        {
+            StreamReader reader = new StreamReader("C:\\Users\\EliteBook\\Desktop\\Input.txt");
+            StreamWriter writer = new StreamWriter("C:\\Users\\EliteBook\\Desktop\\Output.txt");
+            int count = 1;
+            using (reader)
+            {
+                using (writer)
+                {
+                    string line = reader.ReadLine();
+                    while (true)
+                    {
+                        if (line==null)
+                        {
+                            break;
+                        }
+                        writer.WriteLine($"Line{count}: {line}({line.Count(x => char.IsLetter(x))})({line.Count(x => char.IsPunctuation(x))})");
+                        line = reader.ReadLine();
+                        count++;
+                    }
+                }
+            }
+        }
+    }
+}
