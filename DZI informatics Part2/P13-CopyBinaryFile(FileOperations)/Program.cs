7d9bb00568317c8bf5f97cc487e4d3874038ac57
salihe <salihe.bodeva10@gmail.com>
2023-11-04 22:00:17 +0200

    DZI

---
+++ DZI informatics Part2/P13-CopyBinaryFile(FileOperations)/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+
+namespace P13_CopyBinaryFile_FileOperations_
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            FileStream inputFile = new FileStream("C:\\Users\\EliteBook\\Desktop\\10-paris.jpg", FileMode.Open);
+            FileStream output = new FileStream("C:\\Users\\EliteBook\\Desktop\\NewFile.jpg", FileMode.CreateNew);
+            MemoryStream ms = new MemoryStream();
+            using (inputFile)
+            {
+                inputFile.CopyTo(ms);
+            }
+            byte[] array = ms.ToArray();
+            using (output)
+            {
+                foreach (var item in array)
+                {
+                    output.WriteByte(item);
+                }
+            }
+        }
+    }
+}
