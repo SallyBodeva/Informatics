bac4f3df492c78209008e5c6f8a4664ea678569e
salihe <salihe.bodeva10@gmail.com>
2023-11-08 23:32:34 +0200

    DZI

---
+++ DZI informatics Part2/P17-FurnitureOOP/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace P17_FurnitureOOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
