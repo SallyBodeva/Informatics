d3646ef9884109362d10c19b92a84c783dbac9b1
salihe <salihe.bodeva10@gmail.com>
2023-11-10 23:39:05 +0200

    DZI

--- DZI SQL/SQLExam-15.10.2023.sql
+++ DZI SQL/SQLExam-15.10.2023.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE TouristAgency;
+﻿CREATE DATABASE TouristAgency;
 USE TouristAgency;
 
 CREATE TABLE Countries
@@ -85,3 +85,45 @@ CREATE TABLE HotelsRooms
 			REFERENCES Rooms(Id)
 
 );
+
+--Insert
+
+INSERT INTO Tourists(Name,PhoneNumber,Email,CountryId)
+VALUES 
+('John Rivers','653-551-1555','john.rivers@example.com',6),
+('Adeline Aglaé','122-654-8726','adeline.aglae@example.com',2),
+('Sergio Ramirez','233-465-2876','s.ramirez@example.com',3),
+('Johan Müller','322-876-9826','j.muller@example.com',7),
+('Eden Smith','551-874-2234','eden.smith@example.com',6)
+
+
+INSERT INTO Bookings(ArrivalDate,DepartureDate,AdultsCount,ChildrenCount,TouristId,HotelId,RoomId)
+VALUES
+('2024-03-01','2024-03-11',1,0,21,3,5),
+('2023-12-28','2024-01-06',	2,1,22,13,3),
+('2023-11-15','2023-11-20',1,2,23,19,7)
+
+-- Update
+
+Update Bookings
+SET DepartureDate= DATEADD(DAY,1,DepartureDate)
+WHERE MONTH(DepartureDate)=12 OR YEAR(DepartureDate)=2023;
+
+UPDATE Tourists
+SET Email = null
+WHERE Name LIKE '%MA%';
+
+-- Delete
+
+DELETE FROM Tourists
+WHERE Name LIKE '% Smith';
+
+-- Querying
+
+SELECT FORMAT(ArrivalDate,'yyyy-MM-dd') ArrivalDate,AdultsCount,ChildrenCount FROM Bookings AS b
+JOIN Rooms AS r ON b.RoomId=r.Id
+ORDER BY r.Price DESC, ArrivalDate;
+
+SELECT t.Id,t.Name,t.PhoneNumber FROM Tourists AS t
+WHERE t.Id NOT IN (SELECT TouristId FROM Bookings)
+ORDER BY Name;
