699d3a4ab17e6127b2e46b4fd6a9856e5e542a18
salihe <salihe.bodeva10@gmail.com>
2023-11-14 20:32:07 +0200

    DZI

---
+++ DZI informatics Part2/P21-CryptoMiningSystem/Computer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+public class Computer
+{
+    private int ram;
+
+    public Processor Processor { get; set; }
+    public VideoCard VideoCard { get; set; }
+    public int Ram
+    {
+        get { return ram; }
+        set
+        {
+            if (value<0 || value>32)
+            {
+                throw new ArgumentException("PC Ram cannot be less or equal to 0 and more than 32!");
+            }
+            ram = value;
+        }
+    }
+    public double MinedAmountPerHour { get; set; }
+}
---
+++ DZI informatics Part2/P21-CryptoMiningSystem/User.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+
+public class User
+{
+    private readonly int stars;
+    private string name;
+    private double money;
+
+    public string Name
+    {
+        get { return name; }
+        private set
+        {
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                throw new ArgumentException("Username must not be null or empty!");
+            }
+            name = value;
+        }
+    }
+    public int Stars
+    {
+        get
+        {
+            int result = int.Parse(this.Money.ToString());
+            return (result / 100);
+        }
+    }
+
+    public double Money
+    {
+        get { return money; }
+        set
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("User's money cannot be less than 0!");
+            }
+            money = value;
+        }
+    }
+    public Computer Computer { get; set; }
+}
+
