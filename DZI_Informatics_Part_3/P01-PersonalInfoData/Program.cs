ea2d0935cfc42bc61a685fa7bc8a5b4f6b1d2b1d
salihe <salihe.bodeva10@gmail.com>
2023-12-03 22:11:11 +0200

    DZI

---
+++ DZI_Informatics_Part_3/P01-PersonalInfoData/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace P01_PersonalInfoData
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Dictionary<string, int> citiesCount = new Dictionary<string, int>();
+            StreamReader reader = new StreamReader("C:\\Users\\EliteBook\\Desktop\\input.txt");
+            using (reader)
+            {
+                string line = string.Empty;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    int cityNameIndex = line.LastIndexOf('-');
+                    string cityName = line.Substring(cityNameIndex + 2);
+                    if (citiesCount.ContainsKey(cityName))
+                    {
+                        citiesCount[cityName]++;
+                    }
+                    else
+                    {
+                        citiesCount.Add(cityName, 1);
+                    }
+                }
+            }
+            KeyValuePair<string, int> theCity = citiesCount.OrderByDescending(x => x.Value).First();
+            Console.WriteLine($"{theCity.Key} {theCity.Value}");
+        }
+    }
+}
