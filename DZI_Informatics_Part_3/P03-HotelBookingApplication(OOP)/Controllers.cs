d8fad9437832e9f3dcfb40094dc3b5d8583888a9
salihe <salihe.bodeva10@gmail.com>
2023-12-05 22:40:32 +0200

    DZI

--- DZI_Informatics_Part_3/P03-HotelBookingApplication(OOP)/Controllers.cs
+++ DZI_Informatics_Part_3/P03-HotelBookingApplication(OOP)/Controllers.cs
@@ -23,5 +23,6 @@ namespace P03_HotelBookingApplication_OOP_
             }
         }
         //TODO
+
     }
 }
---
+++ DZI_Informatics_Part_3/P03-HotelBookingApplication(OOP)/Engine.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P03_HotelBookingApplication_OOP_
+{
+    public class Engine
+    {
+        // atidzhe dzhivgova nad vsichko
+    }
+}
