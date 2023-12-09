ea051dfc55febb3e852e61ab1ce35bd955b931b7
salihe <salihe.bodeva10@gmail.com>
2023-12-09 23:00:57 +0200

    DZI

--- DZI_Informatics_Part_3/P05-FilesMethods/Program.cs
+++ DZI_Informatics_Part_3/P05-FilesMethods/Program.cs
@@ -8,11 +8,21 @@ namespace P05_FilesMethods
     {
         static void Main()
         {
+            DeleteFile(@"C:\Users\EliteBook\Desktop\salkaEVelika.txt");
         }
         public static void CreateBlankFile(string fileName)
         {
             File.Create(@$"C:\Users\EliteBook\Desktop\{fileName}.txt");
             Console.WriteLine($"A file created with name {fileName}.txt");
+        }
+        public static void DeleteFile(string path)
+        {
+            if (File.Exists(path))
+            {
+                File.Delete(path);
+                Console.WriteLine("File deleted successfully");
+            }
+            else Console.WriteLine("File is not found");
         }
     }
 }
