885b32bf259448e87b5cbc780ffdf7dc514ec992
salihe <salihe.bodeva10@gmail.com>
2023-12-10 20:10:04 +0200

    DZI

---
+++ DZI_Informatics_Part_3/P06-RotationArray/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Linq;
+
+namespace P06_RotationArray
+{
+    public class Program
+    {
+        static void Main()
+        {
+            int[] array = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+            int n = int.Parse(Console.ReadLine());
+            int index = 0;
+            int lastIndex = array.Length;
+            for (int i = 0; i < n; i++)
+            {
+                int firstElement = array[index];
+                array[index] = array[lastIndex - 1];
+                array[lastIndex- 1] = firstElement;
+                if (index <= array.Length - 1) index++;
+                else index = 0;
+                if (lastIndex-- < 0) lastIndex = 0;
+                else lastIndex--;
+            }
+            for (int i = 0; i < array.Length-1; i++)
+            {
+                Console.Write($"{array[i]}, ");
+            }
+            Console.WriteLine(array[array.Length-1]);
+        }
+    }
+}
