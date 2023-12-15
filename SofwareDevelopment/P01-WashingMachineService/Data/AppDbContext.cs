415f9738a9a7e44abb08ba2e973b2400e78e1102
salihe <salihe.bodeva10@gmail.com>
2023-12-15 21:07:26 +0200

    DZI

---
+++ SofwareDevelopment/P01-WashingMachineService/Data/AppDbContext.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P01_WashingMachineService.Data
+{
+    internal class AppDbContext
+    {
+    }
+}
---
+++ SofwareDevelopment/P01-WashingMachineService/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace P01_WashingMachineService
+{
+    internal class Program
+    {
+        static void Main()
+        {
+
+        }
+    }
+}
