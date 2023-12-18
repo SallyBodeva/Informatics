70eadc5766401da87e17bdf12130896b6c73829f
salihe <salihe.bodeva10@gmail.com>
2023-12-18 21:28:54 +0200

    DZI

--- SofwareDevelopment/P02-Service/Program.cs
+++ SofwareDevelopment/P02-Service/Program.cs
@@ -1,5 +1,5 @@
 ﻿using P02_Service.Data;
-
+using System.Linq;
 namespace P02_Service
 {
     public class Program
@@ -7,14 +7,37 @@ namespace P02_Service
         private static AppDbContext context = new AppDbContext();
         static void Main()
         {
-            //var result = context.Reports.Take(5).Select(x =>
-            //new
-            //{
-            //    CategoryName = x.Category.Name,
-            //    Count = y=>y.S
-            //}).OrderByDescending(x => x.Count).OrderBy(x => x.CategoryName);
-            //Console.WriteLine(string.Join(Environment.NewLine,result));
+            GetUnassignedReports();
+            GetReportsCategories();
+            GetMostReportedCategory();
+            GetBirthdayReport();
+        }
 
+        private static void GetBirthdayReport()
+        {
+            var result = context.Reports
+                            .Where(x => x.OpenDate.Day == x.User.Birthdate.Value.Day &&
+                            x.OpenDate.Month == x.User.Birthdate.Value.Month)
+                            .OrderBy(x => x.User.UserName).ThenBy(x => x.Category.Name)
+                            .Select(x => $"{x.User.UserName} {x.Category.Name}");
+
+            Console.WriteLine(string.Join(Environment.NewLine, result));
+        }
+
+        private static void GetMostReportedCategory()
+        {
+            var result = context.Categories.Take(5)
+                .Where(x => x.Reports.Any())
+                .Select(x =>
+                    new
+                    {
+                        Name = x.Name,
+                        ReportsNum = x.Reports.Count()
+                    }).OrderByDescending(x => x.ReportsNum).ThenBy(x => x.Name);
+            foreach (var r in result)
+            {
+                Console.WriteLine($"{r.Name} {r.ReportsNum}");
+            }
         }
 
         private static void GetReportsCategories()
