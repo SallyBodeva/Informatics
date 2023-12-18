c4d04edb748c6f9ead570d452df203abac3bff0a
salihe <salihe.bodeva10@gmail.com>
2023-12-18 20:26:13 +0200

    DZI

--- SofwareDevelopment/P02-Service/Program.cs
+++ SofwareDevelopment/P02-Service/Program.cs
@@ -7,7 +7,24 @@ namespace P02_Service
         private static AppDbContext context = new AppDbContext();
         static void Main()
         {
-            GetUnassignedReports();
+            //var result = context.Reports.Take(5).Select(x =>
+            //new
+            //{
+            //    CategoryName = x.Category.Name,
+            //    Count = y=>y.S
+            //}).OrderByDescending(x => x.Count).OrderBy(x => x.CategoryName);
+            //Console.WriteLine(string.Join(Environment.NewLine,result));
+
+        }
+
+        private static void GetReportsCategories()
+        {
+            var result = context.Reports
+                .Where(x => x.CategoryId != null)
+                .OrderBy(x => x.Description).ThenBy(x => x.Category.Name)
+                .Select(x => $"{x.Description} {x.Category.Name}").ToList();
+
+            Console.WriteLine(string.Join(Environment.NewLine, result));
         }
 
         private static void GetUnassignedReports()
