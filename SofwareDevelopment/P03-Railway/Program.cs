fc16e6e53c343eddbc1540b255a08e7f06409387
salihe <salihe.bodeva10@gmail.com>
2023-12-26 21:00:58 +0200

    DZI

--- SofwareDevelopment/P03-Railway/Program.cs
+++ SofwareDevelopment/P03-Railway/Program.cs
@@ -11,6 +11,22 @@ namespace P03_Railway
 
         }
 
+        private static void GetRailwayStationsWithoutPassingTrains()
+        {
+            var notScheduledTrains = context.TrainRailwayStations.Select(x => x.RailwayStationId);
+
+            var result = context.RailwayStations.Where(x => notScheduledTrains.All(t => x.Id != t))
+                .Select(x =>
+                    new
+                    {
+                        town = x.Town.Name,
+                        RailwayStationName = x.Name
+                    }
+                ).OrderBy(x => x.town).ThenBy(x => x.RailwayStationName);
+
+            Console.WriteLine(string.Join(Environment.NewLine, result));
+        }
+
         private static void GetPassengersTickets()
         {
             var result = context.Tickets
