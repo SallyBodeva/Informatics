559d9618fcadd93a7b7428e574416a5611356835
salihe <salihe.bodeva10@gmail.com>
2024-01-03 22:41:15 +0200

    DZI

---
+++ DZI_Informatics_Part_3/P08-UpperCaseString/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace P08_UpperCaseString
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string input = Console.ReadLine();
+
+            int openTagIndex = input.IndexOf("e>");
+            int closeTagIndex = input.LastIndexOf("</>");
+
+            string wordToUpperCase = input.Substring(openTagIndex, input.Length - closeTagIndex);
+            Console.WriteLine(wordToUpperCase);
+
+            //TODO
+        }
+    }
+}
