0f0317e4005fae64f5a780c60cb424ef6ed6ddc1
salihe <salihe.bodeva10@gmail.com>
2024-01-14 22:18:14 +0200

    DZI

---
+++ DZI_Informatics_Part_3/P12-Worms&Holes/Program.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections.Generic;
+namespace P12_Worms_Holes
+{
+    public class Program
+    {
+        static void Main()
+        {
+            int[] worms = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            int[] holes = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+            Stack<int> s = new Stack<int>(worms);
+            Queue<int> q = new Queue<int>(holes);
+
+            while (s.Count!=0 && q.Count!=0)
+            {
+                if (q.Peek()==s.Peek())
+                {
+                    s.Pop();
+                    q.Dequeue();
+                }
+                else
+                {
+                    
+                }
+            }
+        }
+    }
+}
