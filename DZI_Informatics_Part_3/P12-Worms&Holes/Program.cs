d3ccb0e76eb30e3ccb0d9abf05b32b775095a470
salihe <salihe.bodeva10@gmail.com>
2024-01-14 22:59:42 +0200

    DZI

--- DZI_Informatics_Part_3/P12-Worms&Holes/Program.cs
+++ DZI_Informatics_Part_3/P12-Worms&Holes/Program.cs
@@ -1,4 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
 namespace P12_Worms_Holes
 {
     public class Program
@@ -7,21 +10,41 @@ namespace P12_Worms_Holes
         {
             int[] worms = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
             int[] holes = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
-            Stack<int> s = new Stack<int>(worms);
-            Queue<int> q = new Queue<int>(holes);
+            Stack<int> wormStack = new Stack<int>(worms);
+            Queue<int> holeQueue = new Queue<int>(holes);
+            int matches = 0;
 
-            while (s.Count!=0 && q.Count!=0)
+            while (wormStack.Count != 0 && holeQueue.Count != 0)
             {
-                if (q.Peek()==s.Peek())
+                if (holeQueue.Peek() == wormStack.Peek())
                 {
-                    s.Pop();
-                    q.Dequeue();
+                    wormStack.Pop();
+                    holeQueue.Dequeue();
+                    matches++;
                 }
                 else
                 {
-                    
+                    holeQueue.Dequeue();
+                    int lastWorm = wormStack.Pop();
+                    if ((lastWorm -= 3) > 0)
+                    {
+                        wormStack.Push(lastWorm);
+                    }
                 }
             }
+
+            Console.WriteLine(matches > 0 ? $"Matches: {matches}" : "There are no matches.");
+
+            if (wormStack.Count == 0)
+            {
+                Console.WriteLine(matches == holes.Length ? "Every worm found a suitable hole!" : "Worms left: none");
+            }
+            else
+            {
+                Console.WriteLine($"Worms left: {string.Join(", ", wormStack)}");
+            }
+
+            Console.WriteLine(holeQueue.Count == 0 ? "Holes left: none" : $"Holes left: {string.Join(", ", holeQueue)}");
         }
     }
 }
