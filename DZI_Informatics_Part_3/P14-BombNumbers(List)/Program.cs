ad6b81a60a52822e369c584fa445e1ab9ecf8094
salihe <salihe.bodeva10@gmail.com>
2024-01-15 19:12:11 +0200

    DZI

---
+++ DZI_Informatics_Part_3/P14-BombNumbers(List)/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace P14_BombNumbers_List_
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> nums = Console.ReadLine().Split(' ').Select(int.Parse).ToList();
+            List<int> bombInfo = Console.ReadLine().Split(' ').Select(int.Parse).ToList(); ;
+            int bomb = bombInfo[0];
+            int radius = bombInfo[1];
+            int bombIndex = 0;
+            try
+            {
+                 bombIndex = nums.IndexOf(bomb);
+                for (int i = 1; i <= radius; i++)
+                {
+                    if (bombIndex - i>=0)
+                    {
+                        nums.RemoveAt(bombIndex - i);
+                    }
+                }
+                 bombIndex = nums.IndexOf(bomb);
+                for (int i = 1; i <= radius; i++)
+                {
+                    if (bombIndex + i<=nums.Count-1)
+                    {
+                        nums.RemoveAt(bombIndex + i);
+                    }
+                }
+                nums.Remove(bomb);
+
+            }
+            catch (Exception ex)
+            {
+
+            }
+            Console.WriteLine(string.Join(" ",nums));
+            Console.WriteLine(nums.Sum());
+        }
+    }
+}
