2224ebe4c8a40f826c0180181e3a8bcde333c78b
salihe <salihe.bodeva10@gmail.com>
2024-01-15 16:42:19 +0200

    DZI

---
+++ DZI_Informatics_Part_3/P13-DZI2023/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace P13_DZI2023
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            List<string> list = new List<string>();
+            while (true)
+            {
+                string[] commands = Console.ReadLine().Split(' ').ToArray();
+                if (commands[0]=="End")
+                {
+                    break;
+                }
+                switch (commands[0])
+                {
+                    case "Add":
+                        for (int i = 1; i < commands.Length; i++)
+                        {
+                            list.Add(commands[i]);
+                        }
+                        break;
+                    case "Update":
+                        foreach (var item in list)
+                        {
+                            if (char.IsLetter(item[0]))
+                            {
+                                string newWord = char.ToUpper(item[0]) + item.Substring(1);
+                                
+                            }
+                        }
+                        break;
+                }
+            }
+        }
+    }
+}
