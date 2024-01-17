52a16db73cd2d3773339a5bc02e95f962f6ba679
salihe <salihe.bodeva10@gmail.com>
2024-01-17 14:42:21 +0200

    DZI

---
+++ DZI_Informatics_Part_3/P18-H.A.D.(OPP)/Entities/Hero.cs
@@ -0,0 +1,59 @@
+﻿using HAD.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P18_H.A.D._OPP_.Entities
+{
+    public class Hero : IHero
+    {
+        private string name;
+        private long strength;
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+            set
+            {
+                name = value;
+            }
+
+        }
+
+        public long Strength
+        {
+            get
+            {
+                return strength;
+            }
+            set
+            {
+                strength = value;
+            }
+        }
+
+        public long Agility => throw new NotImplementedException();
+
+        public long Intelligence => throw new NotImplementedException();
+
+        public long HitPoints => throw new NotImplementedException();
+
+        public long Damage => throw new NotImplementedException();
+
+        public IReadOnlyCollection<IItem> Items => throw new NotImplementedException();
+
+        public void AddItem(IItem item)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void AddRecipe(IRecipe recipe)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ DZI_Informatics_Part_3/P18-H.A.D.(OPP)/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace P18_H.A.D._OPP_
+{
+    public class Program
+    {
+        static void Main()
+        {
+            //var engine = new Engine(null, null, null);
+            //engine.Run();
+        }
+    }
+}
