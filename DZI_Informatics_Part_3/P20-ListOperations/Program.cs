8f52d9c7a01513dd2c335190ecee604751793c5a
salihe <salihe.bodeva10@gmail.com>
2024-01-21 11:12:07 +0200

    DZI

---
+++ DZI_Informatics_Part_3/P20-ListOperations/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace P20_ListOperations
+{
+    public class Program
+    {
+        static void Main()
+        {
+            List<int> wagons = Console.ReadLine().Split(" ").Select(int.Parse).ToList();
+            int maxPassengers = int.Parse(Console.ReadLine());
+            string[] cmd;
+            while (true)
+            {
+                cmd = Console.ReadLine().Split(" ");
+                if (cmd[0]=="end")
+                {
+                    break;
+                }
+                if (cmd[0]=="Add")
+                {
+                    wagons.Add(int.Parse(cmd[1]));
+                }
+                else
+                {
+                    int passengers = int.Parse(cmd[0]);
+                    for (int i = 0; i < wagons.Count; i++)
+                    {
+                        if (wagons[i]+passengers<maxPassengers)
+                        {
+                            wagons[i] += passengers;
+                        }
+                        else
+                        {
+                            //TODO
+
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
