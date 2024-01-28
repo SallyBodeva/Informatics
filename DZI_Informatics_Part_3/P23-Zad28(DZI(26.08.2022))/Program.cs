d85c8b89f12da8df2a7a61bfb66a4fcf048aa444
salihe <salihe.bodeva10@gmail.com>
2024-01-28 23:56:53 +0200

    DZI

---
+++ DZI_Informatics_Part_3/P23-Zad28(DZI(26.08.2022))/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace P23_Zad28_DZI_26._08._2022__
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
