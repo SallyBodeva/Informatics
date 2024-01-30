6c3f6f12e55a7ed81eababad5108203591cc3fab
salihe <salihe.bodeva10@gmail.com>
2024-01-30 19:52:20 +0200

    DZI

---
+++ DZI_Informatics_Part4/P01-RotateMatrix/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace P01_RotateMatrix
+{
+    public  class Program
+    {
+        static void Main()
+        {
+            int[,] matrix = ReadMatix();
+            PrintMatrix(matrix);
+
+
+        }
+        public static int[,] ReadMatix()
+        {
+            int[,] matrix = new int[4, 4];
+            Random rnd = new Random();
+
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    matrix[i, j] = rnd.Next(1, 10); 
+                }
+            }
+            return matrix;
+        }
+        public static void PrintMatrix(int[,] matrix)
+        {
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    Console.Write(matrix[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+
+        }
+    }
+}
