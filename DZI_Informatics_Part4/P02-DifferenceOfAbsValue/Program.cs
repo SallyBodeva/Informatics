3f3b0ef9e1d388cd9bdb8535a69eeff8697c771c
salihe <salihe.bodeva10@gmail.com>
2024-02-01 00:01:43 +0200

    DZI

--- DZI_Informatics_Part4/P02-DifferenceOfAbsValue/Program.cs
+++ DZI_Informatics_Part4/P02-DifferenceOfAbsValue/Program.cs
@@ -4,7 +4,25 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List<int> nums = new List<int>();
+            int n = int.Parse(Console.ReadLine());
+            for (int i = 0; i < n; i++)
+            {
+                int num = int.Parse(Console.ReadLine());
+                nums.Add(num);
+            }
+            int minDiff = int.MinValue;
+            Console.WriteLine(string.Join(" ", nums));
+            List<int> orderedNums = nums.OrderBy(x => x).ToList();
+            Console.WriteLine(string.Join(" ", orderedNums));
+            for (int i = 0; i < orderedNums.Count - 1; i++)
+            {
+                if (minDiff < (orderedNums[i] - orderedNums[i + 1]))
+                {
+                    minDiff = orderedNums[i] - orderedNums[i + 1];
+                }
+            }
+            Console.WriteLine(Math.Abs(minDiff));
         }
     }
 }
