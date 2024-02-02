65d607c7fff5796f7bfc6ccd13b1e8af691b6be7
salihe <salihe.bodeva10@gmail.com>
2024-02-02 23:21:05 +0200

    DZI

---
+++ DZI_Informatics_Part4/P04-ArithmeticProgression/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace P04_ArithmeticProgression
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int a1 = int.Parse(Console.ReadLine());
+            int n = int.Parse(Console.ReadLine());
+            List<int> nums = new List<int>();
+            nums.Add(a1);
+            for (int i = 0; i <= n; i++)
+            {
+                int aEven = nums[i] + 1;
+                nums.Add(aEven);
+                int aOdd = nums[i] * 2;
+                nums.Add(aOdd);
+            }
+            Console.WriteLine(nums[n-1]);
+        }
+    }
+}
