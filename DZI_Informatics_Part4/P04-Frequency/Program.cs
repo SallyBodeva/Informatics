7c47418367712575158e5217e17a08e51c13bdd5
salihe <salihe.bodeva10@gmail.com>
2024-02-03 00:15:23 +0200

    DZI

---
+++ DZI_Informatics_Part4/P04-Frequency/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace P04_Frequency
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string[] words = Console.ReadLine().Split(" ");
+            Dictionary<string, int> frequency = new Dictionary<string, int>();
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (frequency.ContainsKey(words[i]))
+                {
+                    frequency[words[i]]++;
+                }
+                else
+                {
+                    frequency.Add(words[i], 1);
+                }
+            }
+            foreach (var item in frequency)
+            {
+                Console.Write($"{item.Key} {item.Value}; ");
+            }
+        }
+    }
+}
