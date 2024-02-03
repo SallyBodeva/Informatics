5d4f2849cd45136f436ba9d606ee8d548ea2416e
salihe <salihe.bodeva10@gmail.com>
2024-02-04 00:13:11 +0200

    DZI

---
+++ DZI_Informatics_Part4/P06-BannedWords/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace P06_BannedWords
+{
+    public class Program
+    {
+        static void Main()
+        {
+            string[] bannedWords = Console.ReadLine().Split(", ");
+            string text = Console.ReadLine();
+
+            for (int i = 0; i < bannedWords.Length; i++)
+            {
+                text = text.Replace(bannedWords[i], new string('*', bannedWords[i].Length));
+            }
+            Console.WriteLine(text);
+        }
+    }
+}
