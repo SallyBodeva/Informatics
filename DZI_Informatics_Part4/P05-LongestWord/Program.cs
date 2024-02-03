9fd8e7d3409acc051616a15c84230939a9459a43
salihe <salihe.bodeva10@gmail.com>
2024-02-03 23:40:35 +0200

    DZI

---
+++ DZI_Informatics_Part4/P05-LongestWord/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace P05_LongestWord
+{
+    public class Program
+    {
+        static void Main()
+        {
+            string[] words = Console.ReadLine().Split(" ");
+            int maxWordLength = 0;
+            for (int i = 0; i < words.Length; i++)
+            {
+                if (words[i].Length>maxWordLength)
+                {
+                    maxWordLength = words[i].Length;
+                }
+            }
+            Console.WriteLine(maxWordLength);
+        }
+    }
+}
