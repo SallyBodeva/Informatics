a1782dd7b8e66856eaf06eeb3338fa80b7b58f28
salihe <salihe.bodeva10@gmail.com>
2024-02-06 18:07:39 +0200

    DZI

--- DZI_Informatics_Part4/P07-zad27(DZI-20.05.2022)/Program.cs
+++ DZI_Informatics_Part4/P07-zad27(DZI-20.05.2022)/Program.cs
@@ -4,7 +4,39 @@
     {
         static void Main(string[] args)
         {
-
+            List<int> nums = new List<int> { 51, 91 };
+            List<int> newNums = DividedByK(nums, 3);
+            Console.WriteLine(string.Join(" ",newNums));
         }
+        public static List<int> DividedByK(List<int> list, int k)
+        {
+            List<int> noNeededNums = new List<int>();
+            foreach (var item in list)
+            {
+                int sum = 0;
+                int num = item;
+                while (num > 0)
+                {
+
+                    int digit = num % 10;
+                    sum += digit;
+                    num /= 10;
+
+                }
+                if (sum % k == 0)
+                {
+                    noNeededNums.Add(item);
+                }
+            }
+            foreach (var item in noNeededNums)
+            {
+                if (list.Contains(item))
+                {
+                    list.Remove(item);
+                }
+            }
+            return list;
+        }
+
     }
 }
