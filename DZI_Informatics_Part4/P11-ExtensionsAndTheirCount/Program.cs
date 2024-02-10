ed0fdaabaf4cd54dc633d1f73bc184573833659d
salihe <salihe.bodeva10@gmail.com>
2024-02-10 21:50:32 +0200

    DZI

---
+++ DZI_Informatics_Part4/P11-ExtensionsAndTheirCount/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace P11_ExtensionsAndTheirCount
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            string path = @"C:\Users\EliteBook\Desktop\test.txt";
+            Dictionary<string, int> files = new Dictionary<string, int>();
+
+            string text = File.ReadAllText(path);
+
+            Console.WriteLine(text);
+            string[] textArray = text.Split(Environment.NewLine);
+
+            Console.WriteLine(string.Join(" ",textArray));
+        }
+        //TODO
+    }
+}
