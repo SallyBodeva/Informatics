db134586211ed805ad5beeaad0239f246610899a
salihe <salihe.bodeva10@gmail.com>
2024-02-11 23:57:58 +0200

    DZI

---
+++ DZI_Informatics_Part4/P12-PeaksAndBottmsInString/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace P12_PeaksAndBottmsInString
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+
+            int peaksCount = 0;
+            int bottomsCount = 0;
+            for (int i = 1; i < numbers.Length-1; i++)
+            {
+                if (numbers[i] > numbers[i-1] && numbers[i] > numbers[i+1])
+                {
+                    peaksCount++;
+                }
+                if (numbers[i] < numbers[i - 1] && numbers[i] < numbers[i + 1])
+                {
+                    bottomsCount++;
+                }
+            }
+            Console.WriteLine($"{peaksCount} {bottomsCount}");
+        }
+    }
+}
---
+++ DZI_Informatics_Part4/P13-UCN/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace P13_UCN
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string UCN = Console.ReadLine();
+
+            string gender = (UCN[8] % 2 == 0) ? "Man" : "Woman";
+
+            int dateOfBirth = int.Parse(UCN.Substring(4, 2));
+            int monthOfBirth = int.Parse( UCN.Substring(2, 2));
+            string yearOfBirth = string.Empty;
+            if (monthOfBirth>40)
+            {
+                yearOfBirth = "20" + UCN.Substring(0, 2);
+                monthOfBirth -= 40;
+            }
+            if (monthOfBirth>20 && monthOfBirth<40)
+            {
+                yearOfBirth = "19" + UCN.Substring(4, 2);
+                monthOfBirth -= 20;
+            }
+            DateTime birtdate = new DateTime(int.Parse(yearOfBirth), monthOfBirth, dateOfBirth);
+            Console.WriteLine($"{gender} is born on" +
+                $" {dateOfBirth.ToString("00")}/{monthOfBirth.ToString("00")}/{yearOfBirth} " +
+                $"and is {DateTime.UtcNow.Year-birtdate.Year} years old");
+
+        }
+    }
+}
