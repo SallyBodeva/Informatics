47d259ad131ba7026d788bd9a15df580a08f4b22
salihe <salihe.bodeva10@gmail.com>
2024-02-12 18:59:54 +0200

    DZI

---
+++ DZI_Informatics_Part4/P14-RandomPassword/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace P14_RandomPassword
+{
+    public class Program
+    {
+        static void Main()
+        {
+            Random r= new Random();
+            int length = r.Next(8, 17);
+            char[] password = new char[length];
+            char capitalLetter = (char)r.Next('A', 'Z');
+            char notCapitalLetter = (char)r.Next('a', 'z');
+            char[] specialSumbols = { '#', '$', '%', '&' };
+            for (int i = 0; i < password.Length; i++)
+            {
+                password[r.Next(0,length-1)] = 
+            }
+
+        }
+    }
+}
