86a1812db927af0c4b3fff8a33a3f7681895c128
salihe <salihe.bodeva10@gmail.com>
2024-02-18 00:12:40 +0200

    DZI

---
+++ DZI_Informatics_Part4/P16-SharkTaxonomy/Classifier.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P16_SharkTaxonomy
+{
+    public class Classifier
+    {
+        private readonly List<Shark> species = new List<Shark>();
+        private readonly int getCount;
+
+        public Classifier(int capacity)
+        {
+            this.Capacity = capacity;
+        }
+
+        public int Capacity { get; set; }
+        public int GetCount => this.species.Count;
+        public List<Shark> Species
+        {
+            get
+            {
+                return species;
+            }
+        }
+        public void AddShark(Shark shark)
+        {
+            if (this.Capacity>=this.GetCount+1 && !this.species.Any(x=>x.Kind==shark.Kind))
+            {
+                this.species.Add(shark);
+            }
+        }
+        public bool RemoveShark(string kind)
+        {
+            return this.species.Remove(species.FirstOrDefault(x => x.Kind == kind));
+        }
+
+        public string GetLargestShark()
+        {
+            return this.species.OrderByDescending(x => x.Length).FirstOrDefault().ToString();
+        }
+        public double GetAverageLength()
+        {
+            return this.species.Average(x => x.Length);
+        }
+        public string Report()
+        {
+            StringBuilder sb = new StringBuilder();
+            sb.AppendLine($"{this.GetCount} sharks classified:");
+            species.ForEach(x => sb.AppendLine(x.ToString()));
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
--- DZI_Informatics_Part4/P16-SharkTaxonomy/Shark.cs
+++ DZI_Informatics_Part4/P16-SharkTaxonomy/Shark.cs
@@ -8,5 +8,23 @@ namespace P16_SharkTaxonomy
 {
     public class Shark
     {
+        public Shark(string kind, int length, string food, string habitat)
+        {
+            this.Kind = kind;
+            this.Length = length;
+            this.Food = food;
+            this.Habitat = habitat;
+        }
+
+        public string Kind { get; set; }
+        public int Length { get; set; }
+        public string Food { get; set; }
+        public string Habitat { get; set; }
+
+        public override string ToString()
+        {
+            return $"{this.Kind} shark: {this.Length}m long.{Environment.NewLine}Could be spotted in the {this.Habitat}, typical menu: {this.Food}";
+        }
     }
+
 }
