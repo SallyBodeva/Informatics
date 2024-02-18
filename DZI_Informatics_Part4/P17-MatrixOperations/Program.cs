6cfc57ca09269ee444788e1f068fbac3a355ac86
salihe <salihe.bodeva10@gmail.com>
2024-02-18 18:40:29 +0200

    DZI

--- DZI_Informatics_Part4/P17-MatrixOperations/Program.cs
+++ DZI_Informatics_Part4/P17-MatrixOperations/Program.cs
@@ -5,8 +5,10 @@
         static void Main()
         {
             int[] coordinates = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
-            int[,] matrix = ReadMatrix(coordinates[0], coordinates[1]);
-            SumMatrixColumns(matrix);
+            int[][] matrix = ReadJaggedArray(coordinates[0]);
+            int[][] newMatrfxi = JaggedArrayModification(matrix);
+            PrintJaggedArray(newMatrfxi);
+
         }
         public static int[,] ReadMatrix(int rows, int cols)
         {
@@ -21,6 +23,37 @@
             }
             return matrix;
         }
+        public static int[][] ReadJaggedArray(int n)
+        {
+            int[][] array = new int[n][];
+            for (int i = 0; i < n; i++)
+            {
+                array[i] = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+            }
+            return array;
+        }
+        public static void PrintJaggedArray(int[][] matrix)
+        {
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix[i].Length; j++)
+                {
+                    Console.Write($"{matrix[i][j]} ");
+                }
+                Console.WriteLine();
+            }
+        }
+        public static void PrintMatrix(int[,] matrix)
+        {
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    Console.Write($"{matrix[i, j]} ");
+                }
+                Console.WriteLine();
+            }
+        }
         public static void SumMatrixColumns(int[,] matrix)
         {
             for (int j = 0; j < matrix.GetLength(1); j++)
@@ -29,12 +62,12 @@
                 for (int i = 0; i < matrix.GetLength(0); i++)
                 {
                     sum += matrix[i, j];
-            
+
                 }
-                  Console.WriteLine(sum);
+                Console.WriteLine(sum);
             }
 
-          
+
         }
         public static int SumMatrixElements(int[,] matrix)
         {
@@ -48,6 +81,75 @@
             }
             return sum;
         }
+        public static int PrimaryDiagonal(int[,] matrix)
+        {
+            int sum = 0;
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    if (i == j)
+                    {
+                        sum += matrix[i, j];
+                    }
+                }
+            }
+            return sum;
+        }
+        public static char[][] ReadCharArray(int n)
+        {
+            char[][] array = new char[n][];
+            for (int i = 0; i < n; i++)
+            {
+                string line = Console.ReadLine();
+                array[i] = line.ToCharArray();
+            }
+            return array;
+        }
+        public static string SymbolInMatrix(char[][] matrix)
+        {
+            char symbol = char.Parse(Console.ReadLine());
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix[i].Length; j++)
+                {
+                    if (matrix[i][j] == symbol)
+                    {
+                        return $"({i}, {j})";
+                    }
+                }
+            }
+            return $"{symbol} does not occur in the matrix";
+        }
+        public static int[][] JaggedArrayModification(int[][] matrix)
+        {
+            string cmd = string.Empty;
+            while ((cmd = Console.ReadLine()) != "END")
+            {
+                try
+                {
+                    string[] data = cmd.Split(' ');
+                    switch (data[0])
+                    {
+                        case "Add":
+                            int row = int.Parse(data[1]);
+                            int col = int.Parse(data[2]);
+                            matrix[row][col] += int.Parse(data[3]);
+                            break;
+                        case "Subtract":
+                            int r = int.Parse(data[1]);
+                            int c = int.Parse(data[2]);
+                            matrix[r][c] -= int.Parse(data[3]);
+                            break;
 
+                    }
+                }
+                catch (Exception)
+                {
+                    Console.WriteLine("Invalid coordinates");
+                }
+            }
+            return matrix;
+        }
     }
 }
---
+++ DZI_Informatics_Part4/P18-PascalTriangle/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace P18_PascalTriangle
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[][] pTriangle = new int[n][];
+            int lineLength = 1;
+            for (int i = 0; i < pTriangle.GetLength(0); i++)
+            {
+                pTriangle[i] = new int[lineLength];
+                lineLength++;
+            }
+            for (int i = 0; i < pTriangle.GetLength(0); i++)
+            {
+                for (int j = 0; j < pTriangle[i].Length; j++)
+                {
+                    if (j==0 || j == pTriangle[i].Length-1)
+                    {
+                        pTriangle[i][j] = 1;
+                    }
+                    else
+                    {
+                        pTriangle[i][j] = pTriangle[i - 1][j] + pTriangle[i - 1][j - 1];
+                    }
+                }
+            }
+            PrintJeggedArray(pTriangle);
+        }
+       
+        public static void PrintJeggedArray(int[][] array)
+        {
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                Console.WriteLine(string.Join(" ", array[i]));
+            }
+            
+        }
+    }
+}
