1d5b976ca26901ab01049dd8e7419b18c56bd74a
salihe <salihe.bodeva10@gmail.com>
2024-02-19 23:56:22 +0200

    DZI

--- DZI_Informatics_Part4/P19-ChickenSnack/Program.cs
+++ DZI_Informatics_Part4/P19-ChickenSnack/Program.cs
@@ -1,10 +1,54 @@
 ﻿namespace P19_ChickenSnack
 {
-    internal class Program
+    public class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
-            Console.WriteLine("Hello, World!");
+            int[] money = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+            int[] prices = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+            int mealsCount = 0;
+
+            Stack<int> stackMoney = new Stack<int>(money);
+            Queue<int> queuePrices = new Queue<int>(prices);
+
+            while (stackMoney.Count!=0 && queuePrices.Count!=0)
+            {
+                int lastMoney = stackMoney.Pop();
+                int firstPrice = queuePrices.Dequeue();
+
+                if (lastMoney==firstPrice)
+                {
+                    mealsCount++;
+                }
+                else if(lastMoney>firstPrice)
+                {
+                    mealsCount++;
+                    int diff = lastMoney - firstPrice;
+                    if (stackMoney.Count>0) { stackMoney.Pop(); }
+                    stackMoney.Push(lastMoney + diff);
+                }
+            }
+            if (mealsCount>=4)
+            {
+                Console.WriteLine($"Gluttony of the day! Henry ate {mealsCount} foods.");
+            }
+            else if (queuePrices.Any())
+            {
+                Console.WriteLine($"Henry ate: {mealsCount} foods.");
+            }
+            else if (mealsCount > 1)
+            {
+                Console.WriteLine($"Henry ate: {mealsCount} foods.");
+            }
+            else if (mealsCount==1)
+            {
+                Console.WriteLine($"Henry ate: {mealsCount} food.");
+            }
+            else
+            {
+                Console.WriteLine("Henry remained hungry. He will try next weekend again.");
+            }
         }
     }
 }
+
