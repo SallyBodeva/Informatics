611cd3ce0c2491073242352ae6f245fb26f91a42
salihe <salihe.bodeva10@gmail.com>
2024-02-22 23:16:53 +0200

    DZI

---
+++ DZI_Informatics_Part4/P22-CountUppercaseWords/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace P22_CountUppercaseWords
+{
+    public class Program
+    {
+        static void Main()
+        {
+            string[] info = File.ReadAllText(@"C:\Users\EliteBook\Desktop\Informatics\DZI_Informatics_Part4\P22-CountUppercaseWords\Info.txt").Split(" ");
+            List<string> upperCaseWords =new List<string>();
+            foreach (var word in info)
+            {
+                if (char.IsUpper(word[0]))
+                {
+                    upperCaseWords.Add(word);
+                }
+            }
+            Console.WriteLine(string.Join(" ",upperCaseWords));
+        }
+    }
+}
