bc0cfa2a81d225075c789094efa7d4a8802f502e
salihe <salihe.bodeva10@gmail.com>
2024-02-24 00:56:46 +0200

    DZI

--- DZI_Informatics_Part4/P24-MatrixManipulation/Program.cs
+++ DZI_Informatics_Part4/P24-MatrixManipulation/Program.cs
@@ -1,16 +1,17 @@
-﻿namespace P24_MatrixManipulation
+﻿using System.IO;
+
+namespace P24_MatrixManipulation
 
 {
     internal class Program
     {
         static void Main()
         {
-
-
-            Console.WriteLine(string.Join(" ", File.ReadAllLines(@"C:\Users\EliteBook\Desktop\test.txt")[0]));
-            Console.WriteLine(File.ReadAllLines(@"C:\Users\EliteBook\Desktop\test.txt")[0].Split(" ").Length);
+            int[,] matrix = ReadMatrix(@"C:\Users\EliteBook\Desktop\test.txt");
+            Console.WriteLine(IsSymmetric(matrix));
+            Console.WriteLine(DiagonalDiff(matrix));
         }
-        public int[,] ReadMatrix(string pathName)
+        public static int[,] ReadMatrix(string pathName)
         {
             int rows = File.ReadAllLines(pathName).Length;
             int col = File.ReadAllLines(pathName)[0].Split(" ").Length;
@@ -22,6 +23,16 @@
                 {
                     throw new Exception();
                 }
+
+                for (int i = 0; i < matrix.GetLength(0); i++)
+                {
+                    string line = File.ReadAllLines(pathName)[i];
+                    for (int j = 0; j < matrix.GetLength(1); j++)
+                    {
+                        matrix[i, j] = int.Parse(line.Split(' ')[j]);
+                    }
+                }
+
             }
             catch (Exception)
             {
@@ -29,6 +40,75 @@
             }
             return matrix;
 
+        }
+        public static bool IsSymmetric(int[,] matrix)
+        {
+            bool isSemmetric = true;
+            for (int i = 0; i < matrix.GetLength(0); i++)
+            {
+                for (int j = 0; j < matrix.GetLength(1); j++)
+                {
+                    if (i != j && matrix[i, j] != matrix[j,i])
+                    {
+                        isSemmetric = false;
+                        return isSemmetric;
+                    }
+                }
+            }
+            return isSemmetric;
+        }
+        public static int SumFirstMainDiagonal(int[,] matrix) 
+        {
+            int firstDiag = 0;
+            try
+            {
+                if (matrix.GetLength(0) != matrix.GetLength(1))
+                {
+                    throw new Exception();
+                }
+                for (int i = 0; i < matrix.GetLength(0); i++)
+                {
+                    for (int j = 0; j < matrix.GetLength(1); j++)
+                    {
+                        if (i == j)
+                        {
+                            firstDiag += matrix[i, j];
+                        }
+                    }
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Invlid data.");
+            }
+            return firstDiag;
+
+        }
+        public static int SumSecMainDiagonal(int[,] matrix)
+        {
+            int sum = 0;
+            try
+            {
+                if (matrix.GetLength(0) != matrix.GetLength(1))
+                {
+                    throw new Exception();
+                }
+                for (int i = 0; i < matrix.GetLength(0); i++)
+                {
+                    sum += matrix[i, matrix.GetLength(0) - i - 1];
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Incorrect data.");
+            }
+          
+            return sum;
+        }
+
+        public static int DiagonalDiff(int[,] matrix)
+        {
+            return Math.Abs((SumFirstMainDiagonal(matrix)-SumSecMainDiagonal(matrix)));
         }
     }
 }
