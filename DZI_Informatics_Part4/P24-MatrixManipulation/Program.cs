3f641f551c2d4abfd9dd823cbe756947ea2aae04
salihe <salihe.bodeva10@gmail.com>
2024-02-23 23:50:47 +0200

    DZI

---
+++ DZI_Informatics_Part4/P24-MatrixManipulation/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace P24_MatrixManipulation
+
+{
+    internal class Program
+    {
+        static void Main()
+        {
+
+
+            Console.WriteLine(string.Join(" ", File.ReadAllLines(@"C:\Users\EliteBook\Desktop\test.txt")[0]));
+            Console.WriteLine(File.ReadAllLines(@"C:\Users\EliteBook\Desktop\test.txt")[0].Split(" ").Length);
+        }
+        public int[,] ReadMatrix(string pathName)
+        {
+            int rows = File.ReadAllLines(pathName).Length;
+            int col = File.ReadAllLines(pathName)[0].Split(" ").Length;
+            int[,] matrix = null;
+            try
+            {
+                matrix = new int[rows, col];
+                if (rows!=col)
+                {
+                    throw new Exception();
+                }
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Invalid data.");
+            }
+            return matrix;
+
+        }
+    }
+}
