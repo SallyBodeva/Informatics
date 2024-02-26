23f8bf69b2801e8d40fa6bc72c9e601b3b3cf75b
salihe <salihe.bodeva10@gmail.com>
2024-02-26 21:01:52 +0200

    DZI

--- DZI_Informatics_Part4/P27-CustomSortingAlgorithms/Program.cs
+++ DZI_Informatics_Part4/P27-CustomSortingAlgorithms/Program.cs
@@ -4,11 +4,14 @@
     {
         static void Main(string[] args)
         {
-            List<int> nums = new List<int>() {13,2,19,81,23,61,32,-19,56,4};
+            List<int> nums = new List<int>() {15,2,21,81,23,63,32,};
 
-            Console.WriteLine(string.Join(" ",nums.OrderBy(x=>SumOfDigit(x)).ThenBy(x=>x)));
-            Console.WriteLine(string.Join(" ", nums.OrderBy(x => Compare(x))));
-            Console.WriteLine(string.Join(" ",OrderByEvenOdd(nums)));
+            //Console.WriteLine(string.Join(" ",nums.OrderBy(x=>SumOfDigit(x)).ThenBy(x=>x)));
+            //Console.WriteLine(string.Join(" ", nums.OrderBy(x => Compare(x))));
+            //Console.WriteLine(string.Join(" ",OrderByEvenOdd(nums)));
+
+            Console.WriteLine(string.Join(" ",SpecificCustimSort(nums)));
+
         }
         public static int SumOfDigit(int num)
         {
@@ -41,6 +44,31 @@
             {
                 return 1 ;
             }
+        }
+        public static List<int> SwapEvenIndex(List<int> nums)
+        {
+            int midIndex = (0 + nums.Count - 1)/2;
+            for (int i = 0; i < midIndex; i+=2)
+            {
+                int destinationIndex = midIndex + (midIndex - i);
+                int firstNum = nums[i];
+                nums[i] = nums[destinationIndex];
+                nums[destinationIndex] = firstNum;
+            }
+            return nums;
+        }
+        public static List<int> SpecificCustimSort(List<int> nums)
+        {
+            List<int> evenNums = nums.Where(x => x % 2 == 0).OrderBy(x => x).ToList();
+            List<int> divisibleBy3 =  nums.Where(x => x % 3 == 0).OrderBy(x => x).ToList();
+            List<int> divisibleBy5 = nums.Where(x => x % 5 == 0).OrderBy(x => x).ToList();
+            List<int> divisibleBy7 = nums.Where(x => x % 3 == 0).OrderBy(x => x).ToList();
+            List<int> newList = new List<int>();
+            newList.AddRange(evenNums);
+            newList.AddRange(divisibleBy3);
+            newList.AddRange(divisibleBy5);
+            newList.AddRange(divisibleBy7);
+            return newList;
         }
     }
 }
