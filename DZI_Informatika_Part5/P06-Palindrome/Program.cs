102ea4c294b4a3e356d7d614d422271f7853042e
salihe <salihe.bodeva10@gmail.com>
2024-03-12 00:20:02 +0200

    DZI

--- DZI_Informatika_Part5/P06-Palindrome/Program.cs
+++ DZI_Informatika_Part5/P06-Palindrome/Program.cs
@@ -4,7 +4,44 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+			try
+			{
+                string number = Console.ReadLine();
+				int n = int.Parse(number);
+                if (n<0)
+                {
+                    throw new Exception();
+                }
+                Console.WriteLine(number);
+
+                string reversedNum = new string(number.Reverse().ToArray());
+                if (number==reversedNum)
+                {
+                    Console.WriteLine("yes");
+                }
+
+                //char[] array = new char[number.Length];
+                //int insertIndex = 0;
+                //for (int i = number.Length - 1; i >= 0; i--)
+                //{
+                //    array[insertIndex] = number[i];
+                //    insertIndex++;
+                //}
+                //string reversedNumver = string.Join("", array);
+                //if (number == reversedNumver)
+                //{
+                //    Console.WriteLine($"{number} is a palindrome");
+                //}
+                //else
+                //{
+                //    Console.WriteLine($"{number} is not a palindrome");
+                //}
+            }
+            catch (Exception)
+			{
+                Console.WriteLine("Incorrectly entered number");
+            }
         }
     }
 }
+
