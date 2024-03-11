4bfd826722d9972a58cee1fcf64f91de3a6ac387
salihe <salihe.bodeva10@gmail.com>
2024-03-11 23:53:02 +0200

    DZI

---
+++ DZI_Informatika_Part5/P06-Palindrome/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace P06_Palindrome
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
