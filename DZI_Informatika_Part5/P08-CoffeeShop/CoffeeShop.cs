8572ccfbd32750d2027504f42cca5de10ea93787
salihe <salihe.bodeva10@gmail.com>
2024-03-15 23:55:23 +0200

    DZI

--- DZI_Informatika_Part5/P08-CoffeeShop/CoffeeShop.cs
+++ DZI_Informatika_Part5/P08-CoffeeShop/CoffeeShop.cs
@@ -10,13 +10,50 @@ namespace P08_CoffeeShop
     {
         private List<Coffee> coffees;
 
-        public CoffeeShop(string name)
+        public CoffeeShop()
         {
-            this.Name = name;
             coffees = new List<Coffee>();
         }
 
         public string Name { get; set; }
+        public void AddCoffee(string name, double price)
+        {
+            Coffee coffee = new Coffee(name, price);
+            this.coffees.Add(coffee);
+        }
+
+        public double AveragePriceInRange(double start, double end)
+        {
+            List<Coffee> cfs = coffees.Where(x => x.Price > start && x.Price < end).ToList();
+            return cfs.Average(x => x.Price);
+        }
+
+
+        public List<string> FilterCoffeesByPrice(double price)
+        {
+            return coffees.Where(x => x.Price < price).Select(x => x.Type).ToList();
+        }
+
+        public List<Coffee> SortAscendingByType()
+        {
+            return this.coffees.OrderBy(x => x.Type).ToList();
+        }
+
+        public List<Coffee> SortDescendingByPrice()
+        {
+            return this.coffees.OrderByDescending(x => x.Price).ToList();
+        }
+
+        public bool CheckCoffeeIsInCoffeeShop(string name)
+        {
+            return this.coffees.Any(x => x.Type == name);
+        }
+
+        public string[] ProvideInformationAboutAllCoffees()
+        {
+            return coffees.Select(x => x.ToString()).ToArray();
+        }
+
 
     }
 }
---
+++ DZI_Informatika_Part5/P08-CoffeeShop/Engine.cs
@@ -0,0 +1,71 @@
+﻿using P08_CoffeeShop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class Engine
+{
+    public Engine()
+    {
+        Run();
+    }
+
+    private void Run()
+    {
+        CoffeeShop coffeeShop = new CoffeeShop();
+        string result = string.Empty;
+        try
+        {
+            while (true)
+            {
+                string[] command = Console.ReadLine().Split(" ");
+                if (command[0]=="END")
+                {
+                    Environment.Exit(0);
+                }
+                switch (command[0])
+                {
+                    case "Add":
+                        string type = command[1];
+                        double price = double.Parse(command[2]);
+                        coffeeShop.AddCoffee(type, price);
+                        result = $"Added Coffee {type}.";
+                        break;
+                    case "AveragePrice":
+                        double startPrice = double.Parse(command[1]);
+                        double endPrice = double.Parse(command[2]);
+                        result = $"Average result: {coffeeShop.AveragePriceInRange(startPrice, endPrice)}";
+                        break;
+                    case "FilterCoffees":
+                        double filterPrice = double.Parse(command[1]);
+                        result = $"Filtered coffees: {string.Join(" ",coffeeShop.FilterCoffeesByPrice(filterPrice))}";
+                        break;
+                    case "SortByType":
+                        coffeeShop.SortAscendingByType();
+                        break;
+                    case "SortByPrice":
+                        coffeeShop.SortDescendingByPrice();
+                        break;
+                    case "CheckCoffee":
+                        string checkType = command[1];
+                        result = coffeeShop.CheckCoffeeIsInCoffeeShop(checkType) ? result = $"Coffee {checkType} is available." : $"Coffee {checkType} is not available.";
+                        break;
+                    case "Print":
+                        result = string.Join(Environment.NewLine, coffeeShop.ProvideInformationAboutAllCoffees());
+                        break;
+                    default:
+                        result = "Something went wrong!";
+                        break;
+                }
+                Console.WriteLine(result);
+            }
+        }
+        catch (Exception ex)
+        {
+            result = ex.Message;
+        }
+    }
+}
+
