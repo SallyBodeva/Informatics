35f17cc3a54b5c2da2659c74b6d2e5fc974552ee
salihe <salihe.bodeva10@gmail.com>
2024-04-01 00:08:18 +0300

    DZI

--- DZI_Informatika_Part5/P11-Crossroads/Program.cs
+++ DZI_Informatika_Part5/P11-Crossroads/Program.cs
@@ -5,6 +5,7 @@
         int secs = int.Parse(Console.ReadLine());
         int freeWind = int.Parse(Console.ReadLine());
         Queue<string> cars = new Queue<string>();
+        int passedCars = 0;
         string cmd = string.Empty;
         while ((cmd = Console.ReadLine())!="END")
         {
@@ -14,8 +15,26 @@
             }
             else
             {
-               
+                List<char> car = new List<char>(cars.Peek().ToCharArray());
+                for (int i = 0; i < secs; i++)
+                {
+                    if (car.Count<=0)
+                    {
+                        break;
+                    }
+                    car.RemoveAt(0);
+                    passedCars++;
+                    //TODO
+                }
+                if (car.Count>0 && car.Count>freeWind)
+                {
+                    Console.WriteLine("A crash happened!");
+                    Console.WriteLine($"{car} was hit at {car[0]}.");
+                    Environment.Exit(0);
+                }
             }
         }
+        Console.WriteLine("Everyone is safe.");
+        Console.WriteLine($"{passedCars} total cars passed the crossroads.");
     }
 }
