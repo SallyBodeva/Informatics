03c5929fad226023f8a8ee2636ecaeec5ff7724a
salihe <salihe.bodeva10@gmail.com>
2024-03-31 19:47:57 +0300

    DZI

---
+++ DZI_Informatika_Part5/P11-Crossroads/Program.cs
@@ -0,0 +1,21 @@
+﻿public class Program
+{
+    public static void Main()
+    {
+        int secs = int.Parse(Console.ReadLine());
+        int freeWind = int.Parse(Console.ReadLine());
+        Queue<string> cars = new Queue<string>();
+        string cmd = string.Empty;
+        while ((cmd = Console.ReadLine())!="END")
+        {
+            if (cmd!="green")
+            {
+                cars.Enqueue(cmd);
+            }
+            else
+            {
+               
+            }
+        }
+    }
+}
