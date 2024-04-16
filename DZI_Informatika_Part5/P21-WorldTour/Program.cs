9065ef46da2836c306faf03e548fae63e106b020
salihe <salihe.bodeva10@gmail.com>
2024-04-16 21:13:02 +0300

    DZI

---
+++ DZI_Informatika_Part5/P21-WorldTour/Program.cs
@@ -0,0 +1,48 @@
+﻿public class Program
+{
+    public static void Main()
+    {
+        string stops = Console.ReadLine();
+        while (true)
+        {
+            string[] input = Console.ReadLine().Split(":");
+            if (input[0] == "Travel")
+            {
+                Console.WriteLine($"Ready for world tour! Planned stops: {stops}");
+                Environment.Exit(0);
+            }
+
+            string command = input[0];
+            switch (command)
+            {
+                case "Add Stop":
+                    int index = int.Parse(input[1]);
+                    string location = input[2];
+                    if (index >= 0 && index < stops.Length)
+                    {
+                        stops = stops.Insert(index, location);
+                    }
+                    Console.WriteLine(stops);
+                    break;
+
+                case "Remove Stop":
+                    int startIndex = int.Parse(input[1]);
+                    int endIndex = int.Parse(input[2]);
+                    if (startIndex >= 0 && endIndex < stops.Length && startIndex <= endIndex)
+                    {
+                       stops = stops.Remove(startIndex, (endIndex - startIndex) + 1);
+                    }
+                    Console.WriteLine(stops);
+                    break;
+                case "Switch":
+                    string oldLocation = input[1];
+                    string newLocation = input[2];
+                    stops = stops.Replace(oldLocation, newLocation);
+                    Console.WriteLine(stops);
+                    break;
+
+            }
+
+        }
+    }
+}
---
+++ DZI_Informatika_Part5/P22-SecretChat/Program.cs
@@ -0,0 +1,45 @@
+﻿public class Program
+{
+    static void Main()
+    {
+        string concealedMessage = Console.ReadLine();
+        while (true)
+        {
+            string[] input = Console.ReadLine().Split(":|:");
+            if (input[0]== "Reveal")
+            {
+                Console.WriteLine($"You have a new text message: {concealedMessage}");
+                Environment.Exit(0);
+            }
+            string cmd = input[0];
+            switch (cmd)
+            {
+                case "InsertSpace":
+                    int index = int.Parse(input[1]);
+                    concealedMessage = concealedMessage.Insert(index, " ");
+                    Console.WriteLine(concealedMessage);
+                    break;
+                case "Reverse":
+                    string substring = input[1];
+                    if (concealedMessage.Contains(substring))
+                    {
+                        concealedMessage = concealedMessage.Replace(substring, "");
+                        string reversedSubstring = new string(substring.Reverse().ToArray());
+                        concealedMessage = concealedMessage + reversedSubstring;
+                    }
+                    else
+                    {
+                        Console.WriteLine("error");
+                    }
+                    Console.WriteLine(concealedMessage);
+                    break;
+                case "ChangeAll":
+                     substring = input[1];
+                    string replacement = input[2];
+                    concealedMessage = concealedMessage.Replace(substring, replacement);
+                    Console.WriteLine(concealedMessage);
+                    break;
+            }
+        }
+    }
+}
