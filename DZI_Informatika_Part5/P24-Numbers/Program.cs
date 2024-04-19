c51f3ab21ec4c155f0fbb0dd26f6c04158c3147a
salihe <salihe.bodeva10@gmail.com>
2024-04-19 19:18:19 +0300

    DZI

---
+++ DZI_Informatika_Part5/P24-Numbers/Program.cs
@@ -0,0 +1,18 @@
+﻿public class Program
+{
+    static void Main()
+    {
+        List<int> numbers = Console.ReadLine().Split(" ").Select(int.Parse).ToList();
+
+        double avg = numbers.Average();
+        numbers = numbers.Where(x => x > avg).OrderByDescending(x=>x).Take(5).ToList();
+        if (numbers.Any())
+        {
+            Console.WriteLine(string.Join(" ", numbers));   
+        }
+        else
+        {
+            Console.WriteLine("No");
+        }
+    }
+}
---
+++ DZI_Informatika_Part5/P25-ArrayModifier/Program.cs
@@ -0,0 +1,39 @@
+﻿public class Program
+{
+    static void Main()
+    {
+        int[] nums = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+        while (true)
+        {
+            string[] input = Console.ReadLine().Split(" ");
+            string cmd = input[0];
+            switch (cmd)
+            {
+                case "swap":
+                    int index1 = int.Parse(input[1]);
+                    int index2 = int.Parse(input[2]);
+
+                    int firstElement = nums[index1];
+                    nums[index1]= nums[index2];
+                    nums[index2] = firstElement;
+                    break;
+                case "multiply":
+                     index1 = int.Parse(input[1]);
+                     index2 = int.Parse(input[2]);
+
+                    nums[index1] *= nums[index2];
+                    break;
+                case "decrease":
+                    for (int i = 0; i < nums.Length; i++)
+                    {
+                        nums[i] = nums[i] - 1;
+                    }
+                    break;
+                case "end":
+                    Console.WriteLine(string.Join(", ",nums));
+                    Environment.Exit(0);
+                    break;
+            }
+        }
+    }
+}
