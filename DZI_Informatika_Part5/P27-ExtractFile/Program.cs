f11e6ddbeca3e9cb09e074209fbb96054c9f7726
salihe <salihe.bodeva10@gmail.com>
2024-04-21 21:35:51 +0300

    DZI

--- DZI_Informatika_Part5/P27-ExtractFile/Program.cs
+++ DZI_Informatika_Part5/P27-ExtractFile/Program.cs
@@ -2,6 +2,28 @@
 {
 	public static void Main()
 	{
-
+		List<string> usernames = Console.ReadLine().Split(", ").ToList();
+		
+		foreach (var item in usernames)
+		{
+			if (item.Length > 3 && item.Length < 16)
+			{
+				if (IsValid(item))
+				{
+                    Console.WriteLine(item);
+                }
+			}
+		}
+	}
+	public static bool IsValid(string word)
+	{
+		foreach (char c in word)
+		{
+			if (!(char.IsLetterOrDigit(c) || c == '-' || c == '_'))
+			{
+				return false;
+			}
+		}
+		return true;
 	}
 }
