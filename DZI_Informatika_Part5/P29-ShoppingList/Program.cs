78aaf73f17739778a4c33dcca54864222b61dd94
salihe <salihe.bodeva10@gmail.com>
2024-04-22 17:28:38 +0300

    DZI

---
+++ DZI_Informatika_Part5/P29-ShoppingList/Program.cs
@@ -0,0 +1,56 @@
+﻿public class Program
+{
+	static void Main()
+	{
+		List<string> groceries = Console.ReadLine().Split("!").ToList();
+
+		while (true)
+		{
+			string[] input = Console.ReadLine().Split(" ");
+			string end = string.Join(" ", input);
+			if (end == "Go Shopping!")
+			{
+				Console.WriteLine(string.Join(", ", groceries));
+				Environment.Exit(0);
+
+			}
+			string cmd = input[0];
+			switch (cmd)
+			{
+				case "Urgent":
+					string item = input[1];
+					if (!groceries.Contains(item))
+					{
+						groceries.Insert(0, item);
+					}
+					break;
+				case "Unnecessary":
+					item = input[1];
+					if (groceries.Contains(item))
+					{
+						groceries.Remove(item);
+					}
+					break;
+				case "Correct":
+					string oldName = input[1];
+					if (groceries.Contains(oldName))
+					{
+						string newName = input[2];
+						int index = groceries.IndexOf(oldName);
+						groceries.RemoveAt(index);
+						groceries.Insert(index, newName);
+					}
+					break;
+				case "Rearrange":
+					item = input[1];
+					if (groceries.Contains(item))
+					{
+						int index = groceries.IndexOf(item);
+						groceries.RemoveAt(index);
+						groceries.Add(item);
+					}
+					break;
+			}
+		}
+	}
+}
---
+++ DZI_Informatika_Part5/P30-HeartDelivery/Program.cs
@@ -0,0 +1,41 @@
+﻿public class Program
+{
+	public static void Main()
+	{
+		List<int> houses = Console.ReadLine().Split("@").Select(int.Parse).ToList();
+		int cupidIndex = 0;
+		while (true)
+		{
+			string[] commands = Console.ReadLine().Split(" ");
+			if (commands[0]== "Love!")
+			{
+                Console.WriteLine($"Cupid's last position was {cupidIndex}.");
+				if (houses.All(x=>x==0))
+				{
+                    Console.WriteLine($"Mission was successful.");
+                }
+				else
+				{
+                    Console.WriteLine($"Cupid has failed {houses.Where(x=>x>0).Count()} places.");
+                }
+                Environment.Exit(0);
+			}
+			int length = int.Parse(commands[1]);
+			cupidIndex += length;
+			if (cupidIndex>=houses.Count)
+			{
+				cupidIndex = 0;
+			}
+			if (houses[cupidIndex]==0)
+			{
+                Console.WriteLine($"Place {cupidIndex} already had Valentine's day.");
+				continue;
+            }
+			houses[cupidIndex] -= 2;
+			if (houses[cupidIndex] ==0)
+			{
+                Console.WriteLine($"Place {cupidIndex} has Valentine's day.");
+            }
+		}
+	}
+}
