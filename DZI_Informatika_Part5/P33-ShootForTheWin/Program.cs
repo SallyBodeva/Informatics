9d0afc50ae1aa3991d0f223ae912b1ff999ac35b
salihe <salihe.bodeva10@gmail.com>
2024-04-24 01:00:58 +0300

    DZI

---
+++ DZI_Informatika_Part5/P33-ShootForTheWin/Program.cs
@@ -0,0 +1,35 @@
+﻿public class Program
+{
+	public static void Main()
+	{
+		List<int> targets = Console.ReadLine().Split(" ").Select(int.Parse).ToList();
+		int shootTargetsCount = 0;
+		while (true)
+		{
+			string cmd = Console.ReadLine();
+			if (cmd=="End")
+			{
+                Console.WriteLine($"Shot targets: {shootTargetsCount} -> {string.Join(" ",targets)}");
+                Environment.Exit(0);
+			}
+			int index = int.Parse(cmd);
+			if (index>=0 && index<targets.Count  && targets[index]!=-1)
+			{
+				int initialValue = targets[index];
+				targets[index] = -1;
+				shootTargetsCount++;
+				for (int i = 0; i < targets.Count; i++)
+				{
+					if (targets[i] > initialValue && targets[i] != -1)
+					{
+						targets[i] -= initialValue;
+					}
+					else if (targets[i] <= initialValue && targets[i] != -1)
+					{
+						targets[i] += initialValue;
+					}
+				}
+			}
+		}
+	}
+}
