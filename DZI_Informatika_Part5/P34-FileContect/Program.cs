e1127954ae1f99937f6885d452248f3fb27a49f1
salihe <salihe.bodeva10@gmail.com>
2024-04-24 23:30:20 +0300

    DZI

---
+++ DZI_Informatika_Part5/P34-FileContect/Program.cs
@@ -0,0 +1,14 @@
+﻿public class Program
+{
+	static void Main()
+	{
+		string path = Console.ReadLine();
+	
+		var names = Directory.EnumerateFiles(path);
+		foreach (var item in names)
+		{
+          
+        }
+
+    }
+}
