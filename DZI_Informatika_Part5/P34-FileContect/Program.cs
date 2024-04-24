6409bf2e4f972a25d64b351b108137a9c544578b
salihe <salihe.bodeva10@gmail.com>
2024-04-24 23:48:16 +0300

    DZI

--- DZI_Informatika_Part5/P34-FileContect/Program.cs
+++ DZI_Informatika_Part5/P34-FileContect/Program.cs
@@ -2,13 +2,26 @@
 {
 	static void Main()
 	{
-		string path = Console.ReadLine();
-	
+		Dictionary<string, int> filesExtensions = new Dictionary<string, int>();
+		string path = @"C:\Users\EliteBook\Desktop\Нова папка";
+
+
 		var names = Directory.EnumerateFiles(path);
 		foreach (var item in names)
 		{
-          
+			string extension = Path.GetExtension(item);
+			if (filesExtensions.ContainsKey(extension))
+			{
+				filesExtensions[extension]++;
+			}
+			else
+			{
+				filesExtensions.Add(extension, 1);
+			}
         }
-
+		foreach (var item in filesExtensions)
+		{
+            Console.WriteLine($"{item.Key} - {item.Value}");
+        }
     }
 }
