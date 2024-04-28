c273d8c5fdaabacedfd7fbdb1f80e49d6a3f734d
salihe <salihe.bodeva10@gmail.com>
2024-04-28 23:36:46 +0300

    DZI

---
+++ DZI_Informatics_Part6/P01-SinoTheWalker/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace P01_SinoTheWalker
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
