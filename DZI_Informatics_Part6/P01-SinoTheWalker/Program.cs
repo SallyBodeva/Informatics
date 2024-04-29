9431022a717a349234ae2b5de6aaee46d80afa9d
salihe <salihe.bodeva10@gmail.com>
2024-04-30 00:22:41 +0300

    DZI

--- DZI_Informatics_Part6/P01-SinoTheWalker/Program.cs
+++ DZI_Informatics_Part6/P01-SinoTheWalker/Program.cs
@@ -3,15 +3,31 @@
     static void Main()
     {
         int[] time = Console.ReadLine().Split(":").Select(int.Parse).ToArray();
-        TimeOnly timeLeaves = new TimeOnly(time[0], time[1], time[2]);
 
         int steps = int.Parse(Console.ReadLine());
         int secPerStep = int.Parse(Console.ReadLine());
 
-        int additionalTime = steps * secPerStep;
-        timeLeaves.AddMinutes(additionalTime/60);
-        double secs = additionalTime % 60.00;
-        timeLeaves.AddMinutes(secs);
-        Console.WriteLine(timeLeaves);
+        int minutes = (steps * secPerStep) / 60;
+        int secs = (steps * secPerStep) % 60;
+
+        if ((time[1] + minutes)>=60)
+        {
+            time[0] += 1;
+            time[1] = (time[1] + minutes) % 60;
+        }
+        else
+        {
+            time[1] += minutes;
+        }
+        if ((time[2] + secs) >= 60)
+        {
+            time[1] += 1;
+            time[2] = (time[2] + secs) % 60;
+        }
+        else
+        {
+            time[2] += minutes;
+        }
+        Console.WriteLine($"{time[0]}:{time[1]}:{time[2].ToString("00")}");
     }
 }
