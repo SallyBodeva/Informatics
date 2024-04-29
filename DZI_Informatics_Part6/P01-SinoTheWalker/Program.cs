2afa38c6f8c64afbd0d1c4353873bd034e2260a4
salihe <salihe.bodeva10@gmail.com>
2024-04-29 23:53:51 +0300

    DZI

--- DZI_Informatics_Part6/P01-SinoTheWalker/Program.cs
+++ DZI_Informatics_Part6/P01-SinoTheWalker/Program.cs
@@ -1,10 +1,17 @@
-﻿namespace P01_SinoTheWalker
+﻿public class Program
 {
-    internal class Program
+    static void Main()
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
+        int[] time = Console.ReadLine().Split(":").Select(int.Parse).ToArray();
+        TimeOnly timeLeaves = new TimeOnly(time[0], time[1], time[2]);
+
+        int steps = int.Parse(Console.ReadLine());
+        int secPerStep = int.Parse(Console.ReadLine());
+
+        int additionalTime = steps * secPerStep;
+        timeLeaves.AddMinutes(additionalTime/60);
+        double secs = additionalTime % 60.00;
+        timeLeaves.AddMinutes(secs);
+        Console.WriteLine(timeLeaves);
     }
 }
