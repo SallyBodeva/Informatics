406b121f8532811cdf3165c4d91bee0eb8faea86
salihe <salihe.bodeva10@gmail.com>
2024-04-30 23:54:56 +0300

    DZI

---
+++ DZI_Informatics_Part6/P03-ValidEmails/Program.cs
@@ -0,0 +1,21 @@
+﻿public class Program
+{
+    public static void Main()
+    {
+        string path = Console.ReadLine();
+        string[] text = File.ReadAllLines(path);
+
+        HashSet<string> validEmails = new HashSet<string>();
+
+        for (int i = 0; i < text.Length; i++)
+        {
+            foreach (var item in text[i].Split(" "))
+            {
+                if (item.Contains("@") && (item.EndsWith(".com") || item.EndsWith(".org")))
+                {
+                    Console.WriteLine(item);
+                }
+            }
+        }
+    }
+}
