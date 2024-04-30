d5611a05510232df130b6a3ae3d44faa89a0d63a
salihe <salihe.bodeva10@gmail.com>
2024-04-30 21:53:19 +0300

    DZI

---
+++ DZI_Informatics_Part6/P02-ListyIterator/ListyIterator.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public class ListyIterator
+{
+    private readonly List<string> collection;
+
+    public ListyIterator(List<string> collection)
+    {
+        this.collection = collection;
+    }
+}
