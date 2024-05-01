f81acf65211d94931f156556b8695308b14e3de7
salihe <salihe.bodeva10@gmail.com>
2024-05-01 22:17:39 +0300

    DZI

---
+++ DZI_Informatics_Part6/P05-CharactersInRange/Program.cs
@@ -0,0 +1,25 @@
+﻿public class Program
+{
+    public static void Main()
+    {
+        char firstSymbol = char.Parse(Console.ReadLine());
+        char nextFirst = (char)((int)firstSymbol + 1);
+        char secSymbol = char.Parse(Console.ReadLine());
+        char nextSec = (char)((int)secSymbol + 1);
+        if (nextFirst < secSymbol)
+        {
+            for (char i = nextFirst; i < secSymbol; i++)
+            {
+                Console.Write(i + " ");
+            }
+        }
+        else
+        {
+            for (char i = nextSec; i < firstSymbol; i++)
+            {
+                Console.Write(i + " ");
+            }
+        }
+       
+    }
+}
---
+++ DZI_Informatics_Part6/P06-PasswordValidator/Program.cs
@@ -0,0 +1,27 @@
+﻿public class Program
+{
+    static void Main()
+    {
+        string password = Console.ReadLine();
+        bool isValid = true;
+        if (password.Length < 6 || password.Length > 10)
+        {
+            Console.WriteLine("Password must be between 6 and 10 characters");
+            isValid = false;
+        }
+        if (password.Any(x => !char.IsDigit(x) && !char.IsLetter(x)))
+        {
+            Console.WriteLine("Password must consist only of letters and digits");
+            isValid = false;
+        }
+        if (password.Count(x => char.IsDigit(x)) < 2)
+        {
+            Console.WriteLine("Password must have at least 2 digits");
+            isValid = false;
+        }
+        if (isValid)
+        {
+            Console.WriteLine("Password is valid");
+        }
+    }
+}
