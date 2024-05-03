d660ee8e690f5af15e6cf1e118a7d66f5e61d97d
salihe <salihe.bodeva10@gmail.com>
2024-05-03 18:50:43 +0300

    DZI

---
+++ DZI_Informatics_Part6/P15-ListTrain/Program.cs
@@ -0,0 +1,33 @@
+﻿public class Program
+{
+    static void Main()
+    {
+        List<int> wagons = Console.ReadLine().Split(" ").Select(int.Parse).ToList();
+        int capacity = int.Parse(Console.ReadLine());
+        while (true)
+        {
+            string[] info = Console.ReadLine().Split(" ");
+            string cmd = info[0];
+            switch (cmd)
+            {
+                case "Add":
+                    wagons.Add(int.Parse(info[1]));
+                    break;
+                case "end":
+                    Console.WriteLine(string.Join(" ",wagons));
+                    Environment.Exit(0);
+                    break;
+                default:
+                    int people = int.Parse(cmd);
+                    for (int i = 0; i < wagons.Count; i++)
+                    {
+                        if (wagons[i]+people<=capacity)
+                        {
+                            wagons[i] += people;
+                        }
+                    }
+                    break;
+            }
+        }
+    }
+}
---
+++ DZI_Informatics_Part6/P16-ChangeList/Program.cs
@@ -0,0 +1,28 @@
+﻿public class Program
+{
+    public static void Main()
+    {
+        List<int> elements = Console.ReadLine().Split(" ").Select(int.Parse).ToList();
+        while (true)
+        {
+            string[] input = Console.ReadLine().Split(" ");
+            string cmd = input[0];
+            switch (cmd)
+            {
+                case "Delete":
+                    int target = int.Parse(input[1]);
+                    elements.RemoveAll(x => x == target);
+                    break;
+                case "Insert":
+                    int element = int.Parse(input[1]);
+                    int index = int.Parse(input[2]);
+                    elements.Insert(index,element);
+                    break;
+                case "end":
+                    Console.WriteLine(string.Join(" ",elements));
+                    Environment.Exit(0);
+                    break;
+            }
+        }
+    }
+}
