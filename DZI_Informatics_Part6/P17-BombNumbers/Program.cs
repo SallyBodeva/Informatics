4b94d1c2c99704e5593915d24807d2bd920bf9af
salihe <salihe.bodeva10@gmail.com>
2024-05-03 20:00:16 +0300

    DZI

---
+++ DZI_Informatics_Part6/P17-BombNumbers/Program.cs
@@ -0,0 +1,29 @@
+﻿public class Program
+{
+    public static void Main()
+    {
+        List<int> elements = Console.ReadLine().Split(" ").Select(int.Parse).ToList();
+        int[] bomb = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+        int indexTarget = elements.IndexOf(elements.FirstOrDefault(x => x == bomb[0]));
+        try
+        {
+            for (int i = 1; i <= bomb[1]; i++)
+            {
+                elements.RemoveAt(indexTarget - 1);
+                elements.RemoveAt(indexTarget + 1);
+                indexTarget = elements.IndexOf(elements.FirstOrDefault(x => x == bomb[0]));
+            }
+            elements.Remove(elements.FirstOrDefault(x => x == bomb[0]));
+        }
+        catch (Exception)
+        {
+
+        }
+        finally
+        {
+            Console.WriteLine(string.Join(" ",elements));
+            Console.WriteLine(elements.Sum());
+        }
+
+    }
+}
