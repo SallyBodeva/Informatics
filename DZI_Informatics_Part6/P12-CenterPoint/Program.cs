31844e371d40e977b39a4d8d6e25fd7140e69c61
salihe <salihe.bodeva10@gmail.com>
2024-05-03 11:09:38 +0300

    DZI

---
+++ DZI_Informatics_Part6/P12-CenterPoint/Program.cs
@@ -0,0 +1,15 @@
+﻿public class Program
+{
+    public static void Main()
+    {
+        double x1 = double.Parse(Console.ReadLine());
+        double y1 = double.Parse(Console.ReadLine());
+        double x2 = double.Parse(Console.ReadLine());
+        double y2 = double.Parse(Console.ReadLine());
+
+        double distanceX = Math.Sqrt(Math.Pow(x1,2)+ Math.Pow(y1, 2));
+        double distanceY = Math.Sqrt(Math.Pow(x2, 2) + Math.Pow(y2, 2));
+        string result = distanceX <= distanceY ? $"({x1}, {y1})" : $"({x2}, {y2})";
+        Console.WriteLine(result);
+    }
+}
---
+++ DZI_Informatics_Part6/P13-TribonacciSequence/Program.cs
@@ -0,0 +1,16 @@
+﻿public class Program
+{
+    static void Main()
+    {
+        List<int> sequence = new List<int>();
+        int n = int.Parse(Console.ReadLine());
+        int initialNum = 1;
+        for (int i = 0; i < n; i++)
+        {
+            sequence.Add(initialNum);
+            initialNum = sequence.TakeLast(3).Sum();
+        }
+        Console.WriteLine(string.Join(" ",sequence));
+    }
+}
+
