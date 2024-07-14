a4784fe645fe6d6418ccc4e378decac15ce776d5
salihe <salihe.bodeva10@gmail.com>
2024-07-14 16:34:49 +0300

    OOP-Exercising

---
+++ C# OOP Exam-10.Dec.2022/Models/Booths/Booth.cs
@@ -0,0 +1,25 @@
+﻿using ChristmasPastryShop.Models.Booths.Contracts;
+using ChristmasPastryShop.Models.Cocktails.Contracts;
+using ChristmasPastryShop.Models.Delicacies.Contracts;
+using ChristmasPastryShop.Repositories.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChristmasPastryShop.Models.Booths
+{
+    public abstract class Booth : IBooth
+    {
+        public abstract int BoothId { get; set; }
+        public abstract int Capacity { get; set; }
+        public abstract IRepository<IDelicacy> DelicacyMenu { get; set; }
+        public abstract IRepository<ICocktail> CocktailMenu { get; set; }
+        public abstract double CurrentBill { get; set; }
+        public abstract double Turnover { get; set; }
+        public abstract bool IsReserved { get; set; }
+
+        public abstract void ChangeStatus();
+        public abstract void Charge();
+        public abstract void UpdateCurrentBill(double amount);
+    }
+}
