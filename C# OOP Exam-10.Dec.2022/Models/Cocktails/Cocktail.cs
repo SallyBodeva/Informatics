cf83baccf37831fb3b0856400af3ed8eabfd7296
salihe <salihe.bodeva10@gmail.com>
2024-07-18 23:30:04 +0300

    OOP

---
+++ C# OOP Exam-10.Dec.2022/Models/Cocktails/Cocktail.cs
@@ -0,0 +1,16 @@
+﻿using ChristmasPastryShop.Models.Cocktails.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChristmasPastryShop.Models.Cocktails
+{
+    public class Cocktail : ICocktail
+    {
+        public string Name => throw new NotImplementedException();
+
+        public string Size => throw new NotImplementedException();
+
+        public double Price => throw new NotImplementedException();
+    }
+}
