2f39a6600ce4d5ead02be8e57fd42b8bb3ce18f1
salihe <salihe.bodeva10@gmail.com>
2024-07-19 23:58:43 +0300

    OOP exercise

---
+++ C# OOP Exam-10.Dec.2022/Core/Controller.cs
@@ -0,0 +1,45 @@
+﻿using ChristmasPastryShop.Core.Contracts;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ChristmasPastryShop.Core
+{
+    public class Controller : IController
+    {
+        public string AddBooth(int capacity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string AddCocktail(int boothId, string cocktailTypeName, string cocktailName, string size)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string AddDelicacy(int boothId, string delicacyTypeName, string delicacyName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string BoothReport(int boothId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string LeaveBooth(int boothId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string ReserveBooth(int countOfPeople)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string TryOrder(int boothId, string order)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
