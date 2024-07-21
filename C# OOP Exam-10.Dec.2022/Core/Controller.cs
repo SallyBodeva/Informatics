d10f328af5d1aab8a7213e83be21b5ea5cfa6fac
salihe <salihe.bodeva10@gmail.com>
2024-07-21 21:17:05 +0300

    OOP

--- C# OOP Exam-10.Dec.2022/Core/Controller.cs
+++ C# OOP Exam-10.Dec.2022/Core/Controller.cs
@@ -1,7 +1,12 @@
 ﻿using ChristmasPastryShop.Core.Contracts;
+using ChristmasPastryShop.Models.Booths;
+using ChristmasPastryShop.Models.Booths.Contracts;
+using ChristmasPastryShop.Models.Cocktails;
+using ChristmasPastryShop.Models.Delicacies;
 using ChristmasPastryShop.Repositories;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace ChristmasPastryShop.Core
@@ -12,18 +17,70 @@ namespace ChristmasPastryShop.Core
 
         public string AddBooth(int capacity)
         {
-            throw new NotImplementedException();
-            // this.booths.AddModel
+            int boothId = booths.Models.Count + 1;
+            Booth booth = new Booth(boothId, capacity);
+            booths.AddModel(booth);
+            return string.Format(Utilities.Messages.OutputMessages.NewBoothAdded, boothId, capacity);
         }
 
         public string AddCocktail(int boothId, string cocktailTypeName, string cocktailName, string size)
         {
-            throw new NotImplementedException();
+            if (cocktailTypeName != nameof(Hibernation) && cocktailTypeName != nameof(MulledWine))
+            {
+                return string.Format(Utilities.Messages.OutputMessages.InvalidCocktailType, cocktailTypeName);
+            }
+            if (size != "Small" && size != "Middle" && size != "Large")
+            {
+                return string.Format(Utilities.Messages.OutputMessages.InvalidCocktailSize, size);
+            }
+            IBooth booth = booths.Models.FirstOrDefault(x => x.BoothId == boothId);
+            if (booth.CocktailMenu.Models.Any(x => x.Name == cocktailName && x.Size == size))
+            {
+                return string.Format(Utilities.Messages.OutputMessages.CocktailAlreadyAdded, size, cocktailName);
+            }
+            Cocktail cocktail = null;
+            switch (cocktailTypeName)
+            {
+                case nameof(Hibernation):
+                    cocktail = new Hibernation(cocktailName, size);
+                    break;
+                case nameof(MulledWine):
+                    cocktail = new Hibernation(cocktailName, size);
+                    break;
+                default:
+                    throw new Exception("Not valid type of coctail...");
+                    break;
+            }
+            booth.CocktailMenu.AddModel(cocktail);
+            return string.Format(Utilities.Messages.OutputMessages.NewCocktailAdded, size, cocktailName, cocktailTypeName);
         }
 
         public string AddDelicacy(int boothId, string delicacyTypeName, string delicacyName)
         {
-            throw new NotImplementedException();
+            if (delicacyTypeName != nameof(Gingerbread) && delicacyTypeName != nameof(Stolen))
+            {
+                return string.Format(Utilities.Messages.OutputMessages.InvalidDelicacyType, delicacyTypeName);
+            }
+            IBooth booth = booths.Models.FirstOrDefault(x => x.BoothId == boothId);
+            if (booth.DelicacyMenu.Models.Any(x=>x.Name==delicacyName))
+            {
+                return string.Format(Utilities.Messages.OutputMessages.DelicacyAlreadyAdded,delicacyName);
+            }
+            Delicacy delicacy = null;
+            switch (delicacyTypeName)
+            {
+                case nameof(Gingerbread):
+                    delicacy = new Gingerbread(delicacyName);
+                    break;
+                case nameof(Stolen):
+                    delicacy = new Stolen(delicacyName);
+                    break;
+                default:
+                    throw new Exception("Not valid type of delicacy...");
+                    break;
+            }
+            booth.DelicacyMenu.AddModel(delicacy);
+            return string.Format(Utilities.Messages.OutputMessages.NewDelicacyAdded,delicacyTypeName,delicacyName);
         }
 
         public string BoothReport(int boothId)
